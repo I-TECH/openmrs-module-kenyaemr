3213a5ca867aae42fd02450c201aa9fc4043d20d
Rowan Seymour <rowanseymour@gmail.com>
2013-01-20 11:34:43 +0200

    Updated MFL sync script to correctly set the MFL code on each new location

--- misc/groovyscripts/mfl_sync.groovy
+++ misc/groovyscripts/mfl_sync.groovy
@@ -26,8 +26,8 @@ numUpdated = 0
  * of that class which matches argClassNames
  */
 def _new(className, argClassNames, Object... args) {
-    def argClasses = (Class[])argClassNames.collect { Context.loadClass(it) }
-    Context.loadClass(className).getDeclaredConstructor(argClasses).newInstance((Object[])args) 
+	def argClasses = (Class[])argClassNames.collect { Context.loadClass(it) }
+	Context.loadClass(className).getDeclaredConstructor(argClasses).newInstance((Object[])args)
 }
 
 /**
@@ -36,73 +36,75 @@ def _new(className, argClassNames, Object... args) {
 def cellValue(cell) { cell.getCellType() == 0 ? cell.getNumericCellValue() : cell.getStringCellValue() }
 
 def importXls(input) {
-    def poifs = _new "org.apache.poi.poifs.filesystem.POIFSFileSystem", [ "java.io.InputStream" ], input
-    def wbook = _new "org.apache.poi.hssf.usermodel.HSSFWorkbook", [ "org.apache.poi.poifs.filesystem.POIFSFileSystem" ], poifs
-    def sheet = wbook.getSheetAt(0)
-     
-    for (def r = sheet.getFirstRowNum() + 1; r <= sheet.getLastRowNum(); ++r) {
-        def row = sheet.getRow(r)
-        def code = String.valueOf((int)cellValue(row.getCell(0)))
-        def name = cellValue(row.getCell(1))
-        def province = cellValue(row.getCell(2))
+	def poifs = _new "org.apache.poi.poifs.filesystem.POIFSFileSystem", [ "java.io.InputStream" ], input
+	def wbook = _new "org.apache.poi.hssf.usermodel.HSSFWorkbook", [ "org.apache.poi.poifs.filesystem.POIFSFileSystem" ], poifs
+	def sheet = wbook.getSheetAt(0)
 
-        if (code && name) {
-            importLocation(code, name, province)
-        }
-    }
+	for (def r = sheet.getFirstRowNum() + 1; r <= sheet.getLastRowNum(); ++r) {
+		def row = sheet.getRow(r)
+		def code = String.valueOf((int)cellValue(row.getCell(0)))
+		def name = cellValue(row.getCell(1))
+		def province = cellValue(row.getCell(2))
+
+		if (code && name) {
+			importLocation(code, name, province)
+		}
+	}
 }
 
 /**
  * Imports a location
  */
 def importLocation(code, name, province) {
-    def location = null
+	def location = null
 
-    // Look for existing location with this code
-    def existingLocationId = mfcToIdMap[code]
-    if (existingLocationId) {
-        location = svc.getLocation(existingLocationId)
-    }
+	// Look for existing location with this code
+	def existingLocationId = mfcToIdMap[code]
+	if (existingLocationId) {
+		location = svc.getLocation(existingLocationId)
+	}
 
-    // Create new location if it doesn't exist
-    if (!location) {
-        location = new org.openmrs.Location()
-    
-        def mfcAttr = new org.openmrs.LocationAttribute()
-        mfcAttr.setAttributeType(mfcAttrType)
-        mfcAttr.setValue(code)
-        mfcAttr.setOwner(location)
+	// Create new location if it doesn't exist
+	if (!location) {
+		location = new org.openmrs.Location()
 
-        numCreated++
-    }
-    else {
-        numUpdated++
-    }
+		def mfcAttr = new org.openmrs.LocationAttribute()
+		mfcAttr.setAttributeType(mfcAttrType)
+		mfcAttr.setValue(code)
+		mfcAttr.setOwner(location)
 
-    location.setName(name)
-    location.setDescription("")
-    location.setCountry("Kenya")
-    location.setStateProvince(province)
+		location.addAttribute(mfcAttr)
 
-    location = svc.saveLocation(location)
-    mfcToIdMap.put(code, location.getLocationId())
+		numCreated++
+	}
+	else {
+		numUpdated++
+	}
 
-    Context.flushSession()
-    Context.clearSession()
+	location.setName(name)
+	location.setDescription("")
+	location.setCountry("Kenya")
+	location.setStateProvince(province)
+
+	location = svc.saveLocation(location)
+	mfcToIdMap.put(code, location.getLocationId())
+
+	Context.flushSession()
+	Context.clearSession()
 }
 
 // Initialize MFC > ID map
-println "Loading MFC to ID mappings..." 
+println "Loading MFC to ID mappings..."
 for (def loc : svc.getAllLocations()) {
-    for (def attr : loc.getActiveAttributes(mfcAttrType)) {
-        if (attr.getValue()) {
-            mfcToIdMap.put(attr.getValue(), loc.getLocationId())
-        }
-    }
+	for (def attr : loc.getActiveAttributes(mfcAttrType)) {
+		if (attr.getValue()) {
+			mfcToIdMap.put(attr.getValue(), loc.getLocationId())
+		}
+	}
 }
 
 // Download zip to temporary file
-println "Downloading MFL. This may take a few minutes..." 
+println "Downloading MFL. This may take a few minutes..."
 def tmpZip = File.createTempFile("mfl", ".zip");
 def out = new BufferedOutputStream(new FileOutputStream(tmpZip));
 out << UPDATE_URL.openStream()
@@ -111,10 +113,10 @@ out.close()
 // Import any contained xls file
 println "Extracting xls from zip file..."
 def zipFile = new java.util.zip.ZipFile(tmpZip)
-zipFile.entries().each { entry -> 
-    if (entry.name.endsWith(".xls")) {
-        importXls(zipFile.getInputStream(entry))
-    }
+zipFile.entries().each { entry ->
+	if (entry.name.endsWith(".xls")) {
+		importXls(zipFile.getInputStream(entry))
+	}
 }
 
 // Delete temp zip file
