f6a54994948d413ab29255b971cfa254fa14c68b
Rowan Seymour <rowanseymour@gmail.com>
2013-01-31 17:05:43 +0200

    Workaronud for META-316 - put MFC location attribute back into the core package

--- api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
@@ -40,7 +40,7 @@ import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
  */
 public class KenyaEmrActivator implements ModuleActivator {
 
-	private static final String METADATA_FILENAME_CORE = "Kenya_EMR_Core_Metadata-14.zip";
+	private static final String METADATA_FILENAME_CORE = "Kenya_EMR_Core_Metadata-15.zip";
 	private static final String METADATA_FILENAME_ROLES = "Kenya_EMR_Roles_and_Privileges-2.zip";
 	private static final String METADATA_FILENAME_LOCATIONS = "Kenya_EMR_Locations-6.zip";
 	private static final String METADATA_FILENAME_FORMS = "Kenya_EMR_Forms-36.zip";
