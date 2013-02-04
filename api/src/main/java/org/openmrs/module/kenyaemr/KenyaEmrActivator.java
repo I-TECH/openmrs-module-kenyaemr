59d0550bbc77f7444d4a829aacc2c9deb9fd4155
ningosi <nicotwendelee@gmail.com>
2013-02-04 12:35:41 +0300

    Duplication fix on forms and minor fixes in the metadata constant

--- api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
@@ -43,7 +43,7 @@ public class KenyaEmrActivator implements ModuleActivator {
 	private static final String METADATA_FILENAME_CORE = "Kenya_EMR_Core_Metadata-15.zip";
 	private static final String METADATA_FILENAME_ROLES = "Kenya_EMR_Roles_and_Privileges-2.zip";
 	private static final String METADATA_FILENAME_LOCATIONS = "Kenya_EMR_Locations-6.zip";
-	private static final String METADATA_FILENAME_FORMS = "Kenya_EMR_Forms-36.zip";
+	private static final String METADATA_FILENAME_FORMS = "Kenya_EMR_Forms-1.zip";
 
 	private static final String REGIMENS_FILENAME = "Kenya_EMR_Regimens.xml";
 
