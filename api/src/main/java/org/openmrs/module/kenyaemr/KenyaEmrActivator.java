db9eee055092cbba52dc5d33fef8964521fd522a
Rowan Seymour <rowanseymour@gmail.com>
2013-02-15 15:49:15 +0200

    Updated metadata packages to get latest concept fix

--- api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/KenyaEmrActivator.java
@@ -40,10 +40,10 @@ import org.openmrs.module.metadatasharing.wrapper.PackageImporter;
  */
 public class KenyaEmrActivator implements ModuleActivator {
 
-	private static final String METADATA_FILENAME_CORE = "Kenya_EMR_Core_Metadata-19.zip";
+	private static final String METADATA_FILENAME_CORE = "Kenya_EMR_Core_Metadata-20.zip";
 	private static final String METADATA_FILENAME_ROLES = "Kenya_EMR_Roles_and_Privileges-2.zip";
 	private static final String METADATA_FILENAME_LOCATIONS = "Kenya_EMR_Locations-6.zip";
-	private static final String METADATA_FILENAME_FORMS = "Kenya_EMR_Forms-11.zip";
+	private static final String METADATA_FILENAME_FORMS = "Kenya_EMR_Forms-12.zip";
 
 	private static final String REGIMENS_FILENAME = "Kenya_EMR_Regimens.xml";
 
