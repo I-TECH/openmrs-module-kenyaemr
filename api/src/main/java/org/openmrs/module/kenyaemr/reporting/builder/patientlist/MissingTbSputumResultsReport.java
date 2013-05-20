bbb28f4e2179890095241e3f248cc7e4104a8611
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2013-05-20 16:41:08 +0300

    Missing TB sputum results and facility based report associated with it added

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/patientlist/MissingTbSputumResultsReport.java
@@ -0,0 +1,19 @@
+package org.openmrs.module.kenyaemr.reporting.builder.patientlist;
+
+import org.openmrs.module.kenyaemr.calculation.tb.MissingTbSputumResultsCalculation;
+
+public class MissingTbSputumResultsReport extends BasePatientCalculationReportBuilder {
+	
+	public MissingTbSputumResultsReport() {
+		super(new MissingTbSputumResultsCalculation());
+	}
+	
+	/**
+	 * @see org.openmrs.module.kenyaemr.reporting.builder.ReportBuilder#getTags()
+	 */
+	@Override
+	public String[] getTags() {
+		return new String[] { "facility", "tb" };
+	}
+
+}
