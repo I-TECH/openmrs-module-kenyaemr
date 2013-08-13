0dae3e27d0ce4c895d89c60bb4d75531b4b10094
gitahi86 <gitahi86@gmail.com>
2013-08-13 15:29:30 +0300

    Added skeleton/dummy java classes, XML form definitions and GSP fragments to handle the MCH program.

---
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/mch/MchEnrollmentSummaryFragmentController.java
@@ -0,0 +1,49 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyaemr.fragment.controller.program.mch;
+
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.PatientProgram;
+import org.openmrs.module.kenyaemr.Dictionary;
+import org.openmrs.module.kenyaemr.util.EmrUtils;
+import org.openmrs.ui.framework.annotation.FragmentParam;
+import org.openmrs.ui.framework.fragment.FragmentModel;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Patient program enrollment fragment
+ */
+public class MchEnrollmentSummaryFragmentController {
+	
+	public String controller(@FragmentParam("patientProgram") PatientProgram enrollment,
+						   @FragmentParam(value = "encounter", required = false) Encounter encounter,
+						   @FragmentParam("showClinicalData") boolean showClinicalData,
+						   FragmentModel model) {
+
+		Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
+		dataPoints.put("Enrolled", enrollment.getDateEnrolled());
+
+		Obs o = EmrUtils.firstObsInProgram(enrollment, Dictionary.getConcept("161655AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));//TODO: Add ANC Concept ID to Dictionary
+		if (o != null) {
+			dataPoints.put("ANC No. ", o.getValueNumeric());
+		}
+
+		model.put("dataPoints", dataPoints);
+		return "view/dataPoints";
+	}
+}
---
+++ omod/src/main/webapp/fragments/program/mch/mchCarePanel.gsp
@@ -0,0 +1,30 @@
+<%
+	ui.decorateWith("kenyaui", "panel", [ heading: "TB Care" ])
+
+	def dataPoints = []
+
+	dataPoints << [ label: "Treatment Number", value: calculations.tbTreatmentNumber ]
+	dataPoints << [ label: "Disease classification", value: calculations.tbDiseaseClassification ]
+	dataPoints << [ label: "Patient classification", value: calculations.tbPatientClassification ]
+%>
+
+<div class="ke-stack-item">
+	<% dataPoints.each { print ui.includeFragment("kenyaui", "widget/dataPoint", it) } %>
+</div>
+<div class="ke-stack-item">
+	<% if (visit) { %>
+		${ ui.includeFragment("kenyaui", "widget/editButton", [ href: ui.pageLink("kenyaemr", "regimenEditor", [ patientId: patient.id, category: "TB", appId: currentApp.id, returnUrl: ui.thisUrl() ]) ]) }
+	<% } %>
+
+	<%
+		if (regimenHistory.lastChange) {
+			def lastChange = regimenHistory.lastChangeBeforeNow
+			def regimen = lastChange.started ? kenyaEmrUi.formatRegimenLong(lastChange.started, ui) : ui.message("general.none")
+			def dateLabel = lastChange.started ? "Started" : "Stopped"
+	%>
+	${ ui.includeFragment("kenyaui", "widget/dataPoint", [ label: "Regimen", value: regimen ]) }
+	${ ui.includeFragment("kenyaui", "widget/dataPoint", [ label: dateLabel, value: lastChange.date, showDateInterval: true ]) }
+	<% } else { %>
+	${ ui.includeFragment("kenyaui", "widget/dataPoint", [ label: "Regimen", value: ui.message("kenyaemr.neverOnTbRegimen") ]) }
+	<% } %>
+</div>
