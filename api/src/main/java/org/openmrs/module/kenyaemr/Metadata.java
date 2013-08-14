4b53572ee278b5cbe2bb47a93fcc8571aec6e695
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2013-08-14 16:27:32 +0300

    Metadata addition - Added encounter and encounter types constants

--- api/src/main/java/org/openmrs/module/kenyaemr/Metadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/Metadata.java
@@ -32,6 +32,11 @@ public class Metadata {
 	public static final String TB_ENROLLMENT_ENCOUNTER_TYPE = "9d8498a4-372d-4dc4-a809-513a2434621e";
 	public static final String TB_SCREENING_ENCOUNTER_TYPE = "ed6dacc9-0827-4c82-86be-53c0d8c449be";
 	public static final String TRIAGE_ENCOUNTER_TYPE = "d1059fb9-a079-4feb-a749-eedd709ae542";
+	public static final String HEI_ENROLLMENT_ENCOUNTER_TYPE = "415f5136-ca4a-49a8-8db3-f994187c3af6";
+	public static final String HEI_DISCONTINUATION_ENCOUNTER_TYPE = "5feee3f1-aa16-4513-8bd0-5d9b27ef1208";
+	public static final String HEI_CONSULTATION_ENCOUNTER_TYPE = "bcc6da85-72f2-4291-b206-789b8186a021";
+	public static final String HEI_IMMUNIZATION_ENCOUNTER_TYPE = "82169b8d-c945-4c41-be62-433dfd9d6c86";
+
 
 	// Forms
 	public static final String HIV_PROGRAM_ENROLLMENT_FORM = "e4b506c1-7379-42b6-a374-284469cba8da";
@@ -49,6 +54,12 @@ public class Metadata {
 	public static final String TB_VISIT_FORM = "2daabb77-7ad6-4952-864b-8d23e109c69d";
 	public static final String TB_COMPLETION_FORM = "4b296dd0-f6be-4007-9eb8-d0fd4e94fb3a";
 	public static final String MOH_257_VISIT_SUMMARY_FORM = "23b4ebbd-29ad-455e-be0e-04aa6bc30798";
+	public static final String HEI_ENROLLMENT_FORM = "8553d869-bdc8-4287-8505-910c7c998aff";
+	public static final String HEI_DISCONTINUATION_FORM = "1dd02c43-904b-4206-8378-7b1a8414c326";
+	public static final String HEI_DISCONTINUATION_FORM = "1dd02c43-904b-4206-8378-7b1a8414c326";
+	public static final String HEI_FOLLOW_UP_FORM = "755b59e6-acbb-4853-abaf-be302039f902";
+	public static final String HEI_IMMUNIZATION_FORM = "b4f3859e-861c-4a63-bdff-eb7392030d47";
+
 
 	// Locations
 	public static final String UNKNOWN_LOCATION = "8d6c993e-c2cc-11de-8d13-0010c6dffd0f";
