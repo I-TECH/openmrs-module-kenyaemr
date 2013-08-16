da21411c685007e008ba6e4f52f5414b81d76f89
gitahi86 <gitahi86@gmail.com>
2013-08-16 18:26:27 +0300

    Added logig to compute period to or since EDD

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/mch/MchEnrollmentSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/mch/MchEnrollmentSummaryFragmentController.java
@@ -14,6 +14,8 @@
 
 package org.openmrs.module.kenyaemr.fragment.controller.program.mch;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 import org.openmrs.Encounter;
 import org.openmrs.Obs;
 import org.openmrs.PatientProgram;
@@ -22,12 +24,17 @@ import org.openmrs.module.kenyaemr.util.EmrUtils;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
  * Patient program enrollment fragment
  */
 public class MchEnrollmentSummaryFragmentController {
+
+	private final int TOO_LONG_AGO = 8;
+	private final int DAYS_IN_WEEK = 7;
 
 	public String controller(@FragmentParam("patientProgram") PatientProgram enrollment,
 							 @FragmentParam(value = "encounter", required = false) Encounter encounter,
@@ -42,16 +49,40 @@ public class MchEnrollmentSummaryFragmentController {
 			dataPoints.put("ANC No. ", ancNoObs.getValueNumeric());
 		}
 		Obs lmpObs = EmrUtils.firstObsInProgram(enrollment, Dictionary.getConcept("1427AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"));
-		;
+		Date edd;
+		String eddPeriod = "";
 		if (lmpObs != null) {
-			dataPoints.put("EDD: ", calculateEdd(lmpObs.getValueDate()));
+			edd = calculateEdd(lmpObs.getValueDate());
+			if (edd != null) {
+				Date today = new Date();
+				int daysBetween = Days.daysBetween(new DateTime(today), new DateTime(edd)).getDays();
+				String suffix;
+				if (daysBetween == 0) {
+					eddPeriod = "TODAY";
+				} else {
+					if (daysBetween > 0) {
+						suffix = "away";
+					} else {
+						suffix = "ago";
+					}
+					daysBetween = Math.abs(daysBetween);
+					int weeks = (int) Math.ceil(daysBetween / DAYS_IN_WEEK);
+					int days = daysBetween % DAYS_IN_WEEK;
+					eddPeriod = formatPeriod(weeks, days, suffix);
+				}
+			}
+			if (edd == null) {
+				dataPoints.put("EDD: ", "Unknown (Missing L.M.P.)");
+			} else {
+				dataPoints.put("EDD: ", new SimpleDateFormat("dd-MM-YYYY").format(edd) + " (" + eddPeriod + ")");
+			}
 		}
-
 		model.put("dataPoints", dataPoints);
 		return "view/dataPoints";
 	}
 
-	private String calculateEdd(Date lmp) {
+	private Date calculateEdd(Date lmp) {
+		Date edd = null;
 		Calendar calendar = new GregorianCalendar();
 		calendar.setTime(lmp);
 		int lmpDay = calendar.get(Calendar.DAY_OF_MONTH);
@@ -63,7 +94,7 @@ public class MchEnrollmentSummaryFragmentController {
 		int yearOffset = 1;
 
 		int eddDay = lmpDay + dayOffset;
-		int dim = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);//daysInMonth(lmpMonth, lmpYear);
+		int dim = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
 		if (eddDay > dim) {
 			eddDay -= dim;
 			monthOffset++;
@@ -74,6 +105,41 @@ public class MchEnrollmentSummaryFragmentController {
 			yearOffset--;
 		}
 		int eddYear = lmpYear + yearOffset;
-		return (eddDay <= 9 ? "0" + eddDay : eddDay) + "-" + (eddMonth <= 9 ? "0" + eddMonth : eddMonth) + "-" + eddYear;
+		try {
+			String eddDateString = (eddDay <= 9 ? "0" + eddDay : eddDay) + "-" + (eddMonth <= 9 ? "0" + eddMonth : eddMonth) + "-" + eddYear;
+			edd = new SimpleDateFormat("dd-MM-yyyy").parse(eddDateString);
+		} catch (ParseException pe) {
+			System.out.println(pe.getMessage());
+		}
+		return edd;
+	}
+
+	private String formatPeriod(int weeksPeriod, int daysPeriod, String suffix) {
+		String periodMessage;
+		if ("ago".equals(suffix) && weeksPeriod > TOO_LONG_AGO) {
+			return "more than " + TOO_LONG_AGO + " weeks ago";
+		}
+		if (weeksPeriod == 0) {
+			if (daysPeriod == 1) {
+				periodMessage = daysPeriod + " day " + suffix;
+			} else {
+				periodMessage = daysPeriod + " days " + suffix;
+			}
+		} else {
+			if (weeksPeriod == 1) {
+				periodMessage = weeksPeriod + " week " + suffix;
+			} else {
+				periodMessage = weeksPeriod + " weeks " + suffix;
+			}
+			if (daysPeriod != 0) {
+				periodMessage = periodMessage.replaceAll(suffix, "");
+				if (daysPeriod == 1) {
+					periodMessage += daysPeriod + " day " + suffix;
+				} else if (daysPeriod > 1) {
+					periodMessage += daysPeriod + " days " + suffix;
+				}
+			}
+		}
+		return periodMessage;
 	}
 }
