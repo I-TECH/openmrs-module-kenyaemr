467a7d7d5c572209abeaf2267ae09736c9d1da88
gitahi86 <gitahi86@gmail.com>
2013-09-11 15:54:43 +0300

    Added 2 flags to the MCH program

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NotOnArtCalculation.java
@@ -0,0 +1,82 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyaemr.calculation.library.mchcs;
+
+import org.openmrs.Concept;
+import org.openmrs.Program;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.kenyacore.calculation.CalculationUtils;
+import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
+import org.openmrs.module.kenyacore.metadata.MetadataUtils;
+import org.openmrs.module.kenyaemr.Dictionary;
+import org.openmrs.module.kenyaemr.Metadata;
+import org.openmrs.module.kenyacore.calculation.BooleanResult;
+import org.openmrs.module.kenyaemr.calculation.BaseEmrCalculation;
+import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Calculates whether a mother is HIV+ but is not on ART. Calculation returns true if mother
+ * is alive, enrolled in the MCH program, is HIV+ and was not indicated as being on ART in the
+ * last encounter.
+ */
+public class NotOnArtCalculation extends BaseEmrCalculation implements PatientFlagCalculation {
+
+	/**
+	 * @see org.openmrs.module.kenyacore.calculation.PatientFlagCalculation#getFlagMessage()
+	 */
+	@Override
+	public String getFlagMessage() {
+		return "Mother not on ART";
+	}
+
+	@Override
+	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
+
+		Program mchmsProgram = MetadataUtils.getProgram(Metadata.Program.MCHMS);
+
+		Set<Integer> alive = alivePatients(cohort, context);
+		Set<Integer> inMchmsProgram = CalculationUtils.patientsThatPass(activeEnrollment(mchmsProgram, alive, context));
+
+		CalculationResultMap lastHivStatusObss = lastObs(getConcept(Dictionary.HIV_STATUS), inMchmsProgram, context);
+		CalculationResultMap artStatusObss = lastObs(getConcept(Dictionary.ANTIRETROVIRAL_USE_IN_PREGNANCY), inMchmsProgram, context);
+
+		CalculationResultMap ret = new CalculationResultMap();
+		for (Integer ptId : cohort) {
+			boolean notOnArt = false;
+
+			// Is patient alive and in MCH program?
+			if (inMchmsProgram.contains(ptId)) {
+				Concept lastHivStatus = EmrCalculationUtils.codedObsResultForPatient(lastHivStatusObss, ptId);
+				Concept lastArtStatus = EmrCalculationUtils.codedObsResultForPatient(artStatusObss, ptId);
+				boolean hivPositive = false;
+				boolean onArt = false;
+				if (lastHivStatus != null) {
+					hivPositive = lastHivStatus.getUuid().equals(Dictionary.POSITIVE);
+					if (lastArtStatus != null) {
+						onArt = !lastArtStatus.getUuid().equals(Dictionary.NOT_APPLICABLE);
+					}
+				}
+				notOnArt = hivPositive && !onArt;
+			}
+			ret.put(ptId, new BooleanResult(notOnArt, this, context));
+		}
+		return ret;
+	}
+}
