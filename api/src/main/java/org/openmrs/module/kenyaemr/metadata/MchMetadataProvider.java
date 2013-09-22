08ed63c03aa5e787ada134893f213919a487f7e5
gitahi86 <gitahi86@gmail.com>
2013-09-22 14:12:02 +0300

    Added MCH-MS forms to the MchMetadataProvider class.

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/MchMetadataProvider.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/MchMetadataProvider.java
@@ -41,6 +41,8 @@ public class MchMetadataProvider extends AbstractMetadataProvider {
 		log.info("Installing MCH metadata");
 
 		installer.program("MCH - Child Services", "Treatment for children", Dictionary.MATERNAL_AND_CHILD_HEALTH_PROGRAM, Metadata.Program.MCHCS);
+
+		//MCH-MS program
 		installer.program("MCH - Mother Services", "Treatment for mothers", Dictionary.MATERNAL_AND_CHILD_HEALTH_PROGRAM, Metadata.Program.MCHMS);
 
 		installer.encounterType("MCH Child Enrollment", "Enrollment of child onto MCH program", Metadata.EncounterType.MCHCS_ENROLLMENT);
@@ -49,8 +51,18 @@ public class MchMetadataProvider extends AbstractMetadataProvider {
 		installer.encounterType("MCH Child Immunization", "Record of child immunizations", Metadata.EncounterType.MCHCS_IMMUNIZATION);
 		installer.encounterType("MCH Child Discontinuation", "Discontinuation of child from MCH program", Metadata.EncounterType.MCHCS_DISCONTINUATION);
 
+		//MCH-MS encounter types
 		installer.encounterType("MCH Mother Enrollment", "Enrollment of mother onto MCH program", Metadata.EncounterType.MCHMS_ENROLLMENT);
 		installer.encounterType("MCH Mother Consultation", "Collection of mother data during MCH visit", Metadata.EncounterType.MCHMS_CONSULTATION);
 		installer.encounterType("MCH Mother Discontinuation", "Discontinuation of mother from MCH program", Metadata.EncounterType.MCHMS_DISCONTINUATION);
+
+		//MCH-MS forms
+		installer.form("MCH-MS Enrollment", "MCH-MS Enrollment", "3ee036d8-7c13-4393-b5d6-036f2fe45126", "1.0", "90a18f0c-17cd-4eec-8204-5af52e8d77cf");
+		installer.form("MCH Antenatal Visit", "MCH antenatal visit form", "c6d09e05-1f25-4164-8860-9f32c5a02df0", "1.0", "e8f98494-af35-4bb8-9fc7-c409c8fed843");
+		installer.form("MCH Postnatal Visit", "MCH postnatal visit form", "c6d09e05-1f25-4164-8860-9f32c5a02df0", "1.0", "72aa78e0-ee4b-47c3-9073-26f3b9ecc4a7");
+		installer.form("Delivery", "MCH-MS delivery form", "c6d09e05-1f25-4164-8860-9f32c5a02df0", "1.0", "496c7cc3-0eea-4e84-a04c-2292949e2f7f");
+		installer.form("Infant Feeding", "MCH-MS infant feeding form", "c6d09e05-1f25-4164-8860-9f32c5a02df0", "1.0", "f4d763bb-8428-476c-be8a-73c705bbc347");
+		installer.form("Preventive Services", "MCH-MS preventive services form", "c6d09e05-1f25-4164-8860-9f32c5a02df0", "1.0", "d3ea25c7-a3e8-4f57-a6a9-e802c3565a30");
+		installer.form("MCH-MS Discontinuation", "MCH-MS discontinuation form", "7c426cfc-3b47-4481-b55f-89860c21c7de", "1.0", "25935b9f-68ad-4e0c-9663-d2cacda82bbf");
 	}
 }
