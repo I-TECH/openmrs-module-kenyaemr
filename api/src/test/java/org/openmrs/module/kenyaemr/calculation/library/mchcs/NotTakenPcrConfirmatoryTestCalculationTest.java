280780833620662f3aeaf868b39c20f4ea6819fd
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2013-09-24 17:38:41 +0300

    Unit test for Infant confirmatory pcr

--- api/src/test/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NotTakenPcrConfirmatoryTestCalculationTest.java
+++ api/src/test/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NotTakenPcrConfirmatoryTestCalculationTest.java
@@ -49,4 +49,7 @@ public class NotTakenPcrConfirmatoryTestCalculationTest extends BaseModuleContex
 	 */
 	@Test
 	public void evaluate_shouldCalculateNotTakenPcrConfirmatoryTest() throws Exception {
+	}
+
+
 }
