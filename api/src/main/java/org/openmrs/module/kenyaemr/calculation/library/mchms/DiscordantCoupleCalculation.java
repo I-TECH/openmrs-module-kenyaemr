538ceb7224a10d97e488b90290cd916ccad99cce
Gitahi Ng'ang'a <gitahi86@gmail.com>
2013-11-20 11:50:07 +0300

    Added calculation to determine discordant couples.

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchms/DiscordantCoupleCalculation.java
@@ -0,0 +1,75 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyaemr.calculation.library.mchms;
+
+import org.openmrs.Concept;
+import org.openmrs.Program;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.module.kenyacore.calculation.BooleanResult;
+import org.openmrs.module.kenyacore.calculation.CalculationUtils;
+import org.openmrs.module.kenyacore.calculation.Calculations;
+import org.openmrs.module.kenyacore.metadata.MetadataUtils;
+import org.openmrs.module.kenyaemr.Dictionary;
+import org.openmrs.module.kenyaemr.calculation.BaseEmrCalculation;
+import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
+import org.openmrs.module.kenyaemr.metadata.MchMetadata;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Determines if a given patient's HIV status is discordant with her male partner's. Discordance means one is positive
+ * while the other is negative.
+ */
+public class DiscordantCoupleCalculation extends BaseEmrCalculation {
+
+	@Override
+	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> params, PatientCalculationContext context) {
+
+		Program mchmsProgram = MetadataUtils.getProgram(MchMetadata._Program.MCHMS);
+
+		Set<Integer> alivePatients = alivePatients(cohort, context);
+		CalculationResultMap activePatientPrograms = Calculations.activeEnrollment(mchmsProgram, alivePatients, context);
+
+		Set<Integer> aliveMchmsPatients = CalculationUtils.patientsThatPass(activePatientPrograms);
+
+		Concept patientHivStatusConcept = getConcept(Dictionary.HIV_STATUS);
+		Concept partnerHivStatusConcept = getConcept(Dictionary.PARTNER_HIV_STATUS);
+
+		CalculationResultMap lastPatientHivStatusObss = Calculations.lastObs(patientHivStatusConcept, aliveMchmsPatients, context);
+		CalculationResultMap lastPartnerHivTestDateObss = Calculations.lastObs(partnerHivStatusConcept, aliveMchmsPatients, context);
+
+		CalculationResultMap resultMap = new CalculationResultMap();
+
+		for (Integer ptId : cohort) {
+			Concept patientLastHivStatus = EmrCalculationUtils.codedObsResultForPatient(lastPatientHivStatusObss, ptId);
+			Concept partnerLastHivStatus = EmrCalculationUtils.codedObsResultForPatient(lastPartnerHivTestDateObss, ptId);
+
+			boolean qualified = false;
+			if (patientLastHivStatus != null && partnerLastHivStatus != null) {
+				Concept positive = Dictionary.getConcept(Dictionary.POSITIVE);
+				Concept negative = Dictionary.getConcept(Dictionary.NEGATIVE);
+				if ((patientLastHivStatus.equals(positive) || patientLastHivStatus.equals(negative))
+						&& (partnerLastHivStatus.equals(positive) || partnerLastHivStatus.equals(negative))) {
+					qualified = !patientLastHivStatus.equals(partnerLastHivStatus);
+				}
+			}
+			resultMap.put(ptId, new BooleanResult(qualified, this, context));
+		}
+		return resultMap;
+	}
+}
