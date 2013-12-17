6c815c923bd6085d5bdd11304b13e2665624acb5
Rowan Seymour <rowanseymour@gmail.com>
2013-12-17 15:08:46 +0200

    Drug autocomplete fields in forms only work for super users (https://www.pivotaltracker.com/story/show/62627484)

--- omod/src/main/java/org/openmrs/module/kenyaemr/EmrExternalUrlInterceptor.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/EmrExternalUrlInterceptor.java
@@ -40,7 +40,11 @@ public class EmrExternalUrlInterceptor extends HandlerInterceptorAdapter {
 	@Autowired
 	protected AdministrationService adminService;
 
-	protected static final String[] BASE_CONTROLLER_WHITELIST = { "org.openmrs.module.kenyaemr", "org.openmrs.module.uiframework" };
+	protected static final String[] BASE_CONTROLLER_WHITELIST = {
+			"org.openmrs.module.htmlformentry.web.controller", // Required for concept widget in HFE
+			"org.openmrs.module.kenyaemr",
+			"org.openmrs.module.uiframework"
+	};
 
 	protected Set<String> controllerWhitelist = null;
 
