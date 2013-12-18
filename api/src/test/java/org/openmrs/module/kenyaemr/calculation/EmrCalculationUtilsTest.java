9fc5a9fe44e1ae71113f9189d563f66a019a94dd
Rowan Seymour <rowanseymour@gmail.com>
2013-12-18 16:07:24 +0200

    Moved EmrCalculationUtils unit tests to kenyacore as they are now tests of CalculationUtils

--- api/src/test/java/org/openmrs/module/kenyaemr/calculation/EmrCalculationUtilsTest.java
+++ api/src/test/java/org/openmrs/module/kenyaemr/calculation/EmrCalculationUtilsTest.java
@@ -15,6 +15,7 @@
 package org.openmrs.module.kenyaemr.calculation;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.calculation.result.ListResult;
@@ -28,46 +29,6 @@ import java.util.List;
 /**
  * Tests for {@link EmrCalculationUtils}
  */
+@Ignore
 public class EmrCalculationUtilsTest {
-
-	/**
-	 * @see org.openmrs.module.kenyacore.calculation.CalculationUtils#ensureNullResults(org.openmrs.calculation.result.CalculationResultMap, java.util.Collection
-	 */
-	@Test
-	public void ensureNullResults_shouldAddNullsForMissingPatients() {
-
-		CalculationResultMap map = new CalculationResultMap();
-		map.put(7, new BooleanResult(true, null));
-		map.put(999, new BooleanResult(true, null));
-
-		List<Integer> cohort = Arrays.asList(6, 7);
-		CalculationUtils.ensureNullResults(map, cohort);
-
-		// Map should now contain 3 patient ids with null for #6
-		Assert.assertEquals(3, map.size());
-		Assert.assertTrue(map.containsKey(6));
-		Assert.assertEquals(null, map.get(6));
-		Assert.assertTrue((Boolean) map.get(7).getValue());
-		Assert.assertTrue((Boolean) map.get(999).getValue());
-	}
-
-	/**
-	 * @see org.openmrs.module.kenyacore.calculation.CalculationUtils#ensureEmptyListResults(org.openmrs.calculation.result.CalculationResultMap, java.util.Collection
-	 */
-	@Test
-	public void ensureEmptyListResults_shouldAddEmptyListsForMissingPatients() {
-
-		CalculationResultMap map = new CalculationResultMap();
-		map.put(7, new ListResult());
-		map.put(999, new ListResult());
-
-		List<Integer> cohort = Arrays.asList(6, 7);
-		CalculationUtils.ensureEmptyListResults(map, cohort);
-
-		// Map should now contain 3 patient ids with empty list for #6
-		Assert.assertEquals(3, map.size());
-		Assert.assertTrue(map.get(6) instanceof ListResult);
-		Assert.assertTrue(map.get(7) instanceof ListResult);
-		Assert.assertTrue(map.get(999) instanceof ListResult);
-	}
 }
