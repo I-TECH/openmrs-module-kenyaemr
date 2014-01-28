57e93aaa31c6542106764328241cf42e95f2d514
Rowan Seymour <rowanseymour@gmail.com>
2014-01-28 17:09:14 +0200

    Added new update component to fix missing OpenMRS IDs

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/update/FixMissingOpenMrsIds.java
@@ -0,0 +1,68 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
+
+package org.openmrs.module.kenyaemr.update;
+
+import org.openmrs.Location;
+import org.openmrs.Patient;
+import org.openmrs.PatientIdentifier;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.api.PatientService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.idgen.service.IdentifierSourceService;
+import org.openmrs.module.kenyacore.update.MaintenanceUpdate;
+import org.openmrs.module.kenyaemr.api.KenyaEmrService;
+import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.io.PrintWriter;
+
+/**
+ * Prior to 13.3.1, the EditPatientFragmentController appears to have sometimes saved a patient without properly saving
+ * their required OpenMRS ID
+ */
+@Component
+public class FixMissingOpenMrsIds implements MaintenanceUpdate {
+
+	@Autowired
+	private KenyaEmrService kenyaEmrService;
+
+	@Autowired
+	private PatientService patientService;
+
+	@Autowired
+	private IdentifierSourceService idgenService;
+
+	@Override
+	public void run(PrintWriter output) throws Exception {
+		PatientIdentifierType openmrsIdType = MetadataUtils.getPatientIdentifierType(CommonMetadata._PatientIdentifierType.OPENMRS_ID);
+		Location defaultLocation = kenyaEmrService.getDefaultLocation();
+
+		int fixed = 0;
+
+		for (Patient patient : Context.getPatientService().getAllPatients()) {
+			if (patient.getPatientIdentifier(openmrsIdType) == null) {
+				String generated = idgenService.generateIdentifier(openmrsIdType, FixMissingOpenMrsIds.class.getSimpleName());
+				PatientIdentifier generatedOpenmrsId = new PatientIdentifier(generated, openmrsIdType, defaultLocation);
+				patient.addIdentifier(generatedOpenmrsId);
+				patientService.savePatientIdentifier(generatedOpenmrsId);
+				fixed++;
+			}
+		}
+
+		output.print("Fixed " + fixed + " missing OpenMRS IDs");
+	}
+}
