9e62edee703df3c03b8a0c33f92c635bbd8dda08
Gitahi Ng'ang'a <gitahi86@gmail.com>
2014-01-29 16:17:06 +0300

    Improved NotOnArtCalculation to stop loading patient observations individually so that it is more efficient. PT Story: https://www.pivotaltracker.com/story/show/64215706
    (cherry picked from commit 2bbf4993f0d64c267a307051742900236c50b94f)
    
    Conflicts:
    	api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchms/NotOnArtCalculation.java

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchms/NotOnArtCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchms/NotOnArtCalculation.java
@@ -17,25 +17,18 @@ package org.openmrs.module.kenyaemr.calculation.library.mchms;
 import org.joda.time.DateTime;
 import org.joda.time.Weeks;
 import org.openmrs.Concept;
-import org.openmrs.Encounter;
-import org.openmrs.EncounterType;
-import org.openmrs.Obs;
-import org.openmrs.Patient;
 import org.openmrs.Program;
-import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
-import org.openmrs.module.kenyacore.calculation.CalculationUtils;
+import org.openmrs.module.kenyacore.calculation.BooleanResult;
 import org.openmrs.module.kenyacore.calculation.Calculations;
 import org.openmrs.module.kenyacore.calculation.Filters;
 import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.kenyaemr.Dictionary;
-import org.openmrs.module.kenyacore.calculation.BooleanResult;
 import org.openmrs.module.kenyaemr.calculation.BaseEmrCalculation;
 import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
 import org.openmrs.module.kenyaemr.metadata.MchMetadata;
-import org.openmrs.module.kenyaemr.util.EmrUtils;
 
 import java.util.Collection;
 import java.util.Date;
@@ -71,6 +64,7 @@ public class NotOnArtCalculation extends BaseEmrCalculation implements PatientFl
 
 		CalculationResultMap lastHivStatusObss = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_STATUS), inMchmsProgram, context);
 		CalculationResultMap artStatusObss = Calculations.lastObs(Dictionary.getConcept(Dictionary.ANTIRETROVIRAL_USE_IN_PREGNANCY), inMchmsProgram, context);
+		CalculationResultMap lmpObss = Calculations.firstObs(Dictionary.getConcept(Dictionary.LAST_MONTHLY_PERIOD), inMchmsProgram, context);
 
 		CalculationResultMap ret = new CalculationResultMap();
 		for (Integer ptId : cohort) {
@@ -79,6 +73,8 @@ public class NotOnArtCalculation extends BaseEmrCalculation implements PatientFl
 			if (inMchmsProgram.contains(ptId)) {
 				Concept lastHivStatus = EmrCalculationUtils.codedObsResultForPatient(lastHivStatusObss, ptId);
 				Concept lastArtStatus = EmrCalculationUtils.codedObsResultForPatient(artStatusObss, ptId);
+				Date lastLmpDate = EmrCalculationUtils.datetimeObsResultForPatient(lmpObss, ptId);
+
 				boolean hivPositive = false;
 				boolean onArt = false;
 				if (lastHivStatus != null) {
@@ -87,7 +83,7 @@ public class NotOnArtCalculation extends BaseEmrCalculation implements PatientFl
 						onArt = !lastArtStatus.equals(Dictionary.getConcept(Dictionary.NOT_APPLICABLE));
 					}
 				}
-				notOnArt = hivPositive && gestationIsGreaterThan14Weeks(ptId) && !onArt;
+				notOnArt = hivPositive && gestationIsGreaterThan14Weeks(lastLmpDate) && !onArt;
 			}
 			ret.put(ptId, new BooleanResult(notOnArt, this, context));
 		}
@@ -95,15 +91,11 @@ public class NotOnArtCalculation extends BaseEmrCalculation implements PatientFl
 	}
 
 	/**
-	 * @return true if the given patient's gestation is greater than 14 weeks at enrollment and false otherwise
-	 * */
-	protected boolean gestationIsGreaterThan14Weeks(Integer patientId) {
-		Patient patient = Context.getPatientService().getPatient(patientId);
-		EncounterType encounterType = MetadataUtils.getEncounterType(MchMetadata._EncounterType.MCHMS_ENROLLMENT);
-		Encounter lastMchEnrollment = EmrUtils.lastEncounter(patient, encounterType);
-		Obs lmpObs = EmrUtils.firstObsInEncounter(lastMchEnrollment, Dictionary.getConcept(Dictionary.LAST_MONTHLY_PERIOD));
-		if (lmpObs != null) {
-			Weeks weeks = Weeks.weeksBetween(new DateTime(lmpObs.getValueDate()), new DateTime(new Date()));
+	 * @return true if the given patient's gestation is greater than 14 weeks and false otherwise
+	 */
+	protected boolean gestationIsGreaterThan14Weeks(Date lmpDate) {
+		if (lmpDate != null) {
+			Weeks weeks = Weeks.weeksBetween(new DateTime(lmpDate), new DateTime(new Date()));
 			if (weeks.getWeeks() > 14) {
 				return true;
 			}
