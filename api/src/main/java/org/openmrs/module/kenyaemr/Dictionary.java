8a5246778d29c7be7dde5d975cfd307923c21713
Rowan Seymour <rowanseymour@gmail.com>
2014-03-27 10:12:03 +0200

    Moved getConcept implementation from MetadataUtils in metadatadeploy where it is now deprecated

--- api/src/main/java/org/openmrs/module/kenyaemr/Dictionary.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/Dictionary.java
@@ -15,7 +15,10 @@
 package org.openmrs.module.kenyaemr;
 
 import org.openmrs.Concept;
+import org.openmrs.ConceptNumeric;
+import org.openmrs.api.context.Context;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.openmrs.module.metadatadeploy.MissingMetadataException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +35,31 @@ public class Dictionary extends Metadata.Concept {
 	 * @throws org.openmrs.module.metadatadeploy.MissingMetadataException if the concept could not be found
 	 */
 	public static Concept getConcept(String identifier) {
-		return MetadataUtils.getConcept(identifier);
+		Concept concept;
+
+		if (identifier.contains(":")) {
+			String[] tokens = identifier.split(":");
+			concept = Context.getConceptService().getConceptByMapping(tokens[1].trim(), tokens[0].trim());
+		}
+		else {
+			// Assume it's a UUID
+			concept = Context.getConceptService().getConceptByUuid(identifier);
+		}
+
+		if (concept == null) {
+			throw new MissingMetadataException(Concept.class, identifier);
+		}
+
+		// getConcept doesn't always return ConceptNumeric for numeric concepts
+		if (concept.getDatatype().isNumeric() && !(concept instanceof ConceptNumeric)) {
+			concept = Context.getConceptService().getConceptNumeric(concept.getId());
+
+			if (concept == null) {
+				throw new MissingMetadataException(ConceptNumeric.class, identifier);
+			}
+		}
+
+		return concept;
 	}
 
 	/**
