61b61cc20f925d7ee54ffb2cca6e9079a0caaad4
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2014-04-25 17:42:50 +0300

    updated pom to use latest kenyacore and added analysis report builder

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/hiv/ArtCohortAnalysisReportBuilder.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/hiv/ArtCohortAnalysisReportBuilder.java
@@ -1,19 +1,53 @@
 package org.openmrs.module.kenyaemr.reporting.builder.hiv;
 
-import org.openmrs.module.kenyacore.report.CalculationReportDescriptor;
+import org.openmrs.PatientIdentifierType;
+import org.openmrs.module.kenyacore.report.CohortReportDescriptor;
+import org.openmrs.module.kenyacore.report.builder.AbstractCohortReportBuilder;
 import org.openmrs.module.kenyacore.report.builder.Builds;
-import org.openmrs.module.kenyacore.report.builder.CalculationReportBuilder;
+import org.openmrs.module.kenyaemr.metadata.HivMetadata;
+import org.openmrs.module.kenyaemr.reporting.library.shared.hiv.art.ArtCohortLibrary;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
+import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.data.DataDefinition;
+import org.openmrs.module.reporting.data.converter.DataConverter;
+import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
+import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
+import org.openmrs.module.reporting.evaluation.parameter.Mapped;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 @Builds("kenyaemr.hiv.report.artCohortAnalysis")
-public class ArtCohortAnalysisReportBuilder extends CalculationReportBuilder {
+public class ArtCohortAnalysisReportBuilder extends AbstractCohortReportBuilder {
+
+	@Autowired
+	ArtCohortLibrary artCohortLibrary;
 
 	@Override
-	protected void addColumns(CalculationReportDescriptor report, PatientDataSetDefinition dsd) {
+	protected void addColumns(CohortReportDescriptor report, PatientDataSetDefinition dsd) {
 
-		addStandardColumns(report, dsd);
+		PatientIdentifierType upn = MetadataUtils.existing(PatientIdentifierType.class, HivMetadata._PatientIdentifierType.UNIQUE_PATIENT_NUMBER);
+		DataConverter identifierFormatter = new ObjectFormatter("{identifier}");
+		DataDefinition identifierDef = new ConvertedPatientDataDefinition("identifier", new PatientIdentifierDataDefinition(upn.getName(), upn), identifierFormatter);
 
+		dsd.addColumn(upn.getName(), identifierDef, "");
+		dsd.addColumn("Birth date", new BirthdateDataDefinition(), "");
+		dsd.addColumn("Sex", new GenderDataDefinition(), "");
+	}
+
+	@Override
+	protected Mapped<CohortDefinition> buildCohort(CohortReportDescriptor descriptor, PatientDataSetDefinition dsd) {
+		CohortDefinition cd = artCohortLibrary.netCohortMonths(12);
+		Map<String, Object> mappings = new HashMap<String, Object>();
+		mappings.put("ondDate", new Date());
+		return new Mapped<CohortDefinition>(cd, mappings);
 	}
 }
