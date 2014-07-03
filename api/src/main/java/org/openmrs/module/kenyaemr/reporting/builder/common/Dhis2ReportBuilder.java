c605a4f12d4da1d5b42af4bdcb2fb1ddd7d6ffbe
Nicholas Ingosi Magaja <nicotwendelee@gmail.com>
2014-07-03 16:00:02 +0300

    Added dhis report components(cohort,indiacator and builder)

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/common/Dhis2ReportBuilder.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/common/Dhis2ReportBuilder.java
@@ -21,6 +21,7 @@ import org.openmrs.module.kenyacore.report.builder.AbstractReportBuilder;
 import org.openmrs.module.kenyacore.report.builder.Builds;
 import org.openmrs.module.kenyaemr.reporting.ColumnParameters;
 import org.openmrs.module.kenyaemr.reporting.library.shared.common.CommonDimensionLibrary;
+import org.openmrs.module.kenyaemr.reporting.library.shared.hiv.Dhis2IndicatorLibrary;
 import org.openmrs.module.kenyaemr.reporting.library.shared.hiv.art.ArtIndicatorLibrary;
 import org.openmrs.module.reporting.dataset.definition.CohortIndicatorDataSetDefinition;
 import org.openmrs.module.reporting.dataset.definition.DataSetDefinition;
@@ -48,6 +49,10 @@ public class Dhis2ReportBuilder extends AbstractReportBuilder {
 
 	@Autowired
 	private ArtIndicatorLibrary artIndicators;
+
+
+	@Autowired
+	private Dhis2IndicatorLibrary dhis2IndicatorLibrary;
 
 
 	@Override
@@ -82,6 +87,37 @@ public class Dhis2ReportBuilder extends AbstractReportBuilder {
 		ColumnParameters colTotal = new ColumnParameters("TP", "grand total", "");
 
 		String indParams = "startDate=${startDate},endDate=${endDate}";
+
+		//2.6 Infant Testing (initial tests only)
+		dsd.addColumn("PCR-2", "PCR (within 2 months) Infant Testing (initial tests only)", ReportUtils.map(dhis2IndicatorLibrary.pcrInitialWithin2Months(), indParams), "");
+		dsd.addColumn("PCR-3-8", "PCR (from 3 to 8 months)Infant Testing (initial tests only)", ReportUtils.map(dhis2IndicatorLibrary.pcrInitial3To8Months(), indParams), "");
+		dsd.addColumn("SER-9-12", "Serology(from 9 to 12 months) Infant Testing (initial tests only)", ReportUtils.map(dhis2IndicatorLibrary.serologyAntBodyTestBetween9And12Months(), indParams), "");
+		dsd.addColumn("PCR-9-12", "PCR(from 9 to 12 months)Infant Testing (initial test only)", ReportUtils.map(dhis2IndicatorLibrary.pcrTestBetween9And12Months(), indParams), "");
+		dsd.addColumn("HEI-12-T", "Total HEI tested by 12 months", ReportUtils.map(dhis2IndicatorLibrary.totalHeiTestedBy12Months(), indParams), "");
+
+		//2.7 Confirmed Infant Test Results
+		dsd.addColumn("PCR-2-C", "PCR (by 2 months) Confirmed Infant Test Results Positive", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("PCR-3-8-C", "PCR (3 to 8 months) Confirmed Infant Test Results Positive", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositiveBetween3To8Months(), indParams), "");
+		dsd.addColumn("PCR-9-12-C", "PCR (9 to 12 months) Confirmed Infant Test Results Positive", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositiveBetween9To12Months(), indParams), "");
+		dsd.addColumn("PCR-C-T", "Total Confirmed Positive Infant test result by PCR", ReportUtils.map(dhis2IndicatorLibrary.pcrTotalConfirmedPositive(), indParams), "");
+
+		//2.8 Infant Feeding
+		dsd.addColumn("EBF-6", "EBF (6 months) Infant Feeding", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("ERF-6", "ERF (6 months) Infant Feeding", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("MF-6", "MF (6 months) Infant Feeding", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("TE-6", "Total Exposed aged 6 months", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("BF-12", "BF (at 12 months) Infant Feeding", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("NBF-12", "Not BF (12 months) Infant Feeding", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("NKIF-12", "Not Known Infant Feeding (12 months)", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("TE-12", "Total Exposed 12 months", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+
+		//2.9 Infant ARV Prophylaxis(at first contact only)
+		dsd.addColumn("IANC", "Issued in ANC (Infant ARV prophylaxis)", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("LAD", "Labour and Delivery (Infant ARV prophylaxis)", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("PNC", "PNC (<72hrs) (Infant ARV prophylaxis)", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+		dsd.addColumn("TIIP", "Total Infants Issued Prophylaxis", ReportUtils.map(dhis2IndicatorLibrary.pcrConfirmedPositive2Months(), indParams), "");
+
+
 
 		return dsd;
 
---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/library/shared/hiv/Dhis2CohortLibrary.java
@@ -0,0 +1,10 @@
+package org.openmrs.module.kenyaemr.reporting.library.shared.hiv;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Library of Dhis2 related cohort definitions
+ */
+@Component
+public class Dhis2CohortLibrary {
+}
