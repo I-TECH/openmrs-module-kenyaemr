7e7891e5416f7350d5ecd8bcbda59eecd3ff1b07
ojwanganto <ojwangwachiaje@gmail.com>
2015-03-21 10:14:59 +0300

    More fixes for eligibility criteria

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/DateMedicallyEligibleForARTCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/DateMedicallyEligibleForARTCalculation.java
@@ -15,14 +15,12 @@ import org.openmrs.module.kenyaemr.Dictionary;
 import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
 import org.openmrs.module.kenyaemr.calculation.library.hiv.art.InitialArtStartDateCalculation;
 import org.openmrs.module.kenyaemr.metadata.HivMetadata;
+import org.openmrs.module.kenyaemr.reporting.model.PatientEligibility;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.reporting.common.Age;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -48,7 +46,7 @@ public class DateMedicallyEligibleForARTCalculation extends AbstractPatientCalcu
         CalculationResultMap artStartDateMap = calculate(new InitialArtStartDateCalculation(), cohort, context);
 
         for (Integer ptId : cohort) {
-            String dateAndReason = null;
+            PatientEligibility eligibilityInfo = null;
             if (inHivProgram.contains(ptId)) {
                 int ageInMonths = ((Age) ages.get(ptId).getValue()).getFullMonths();
                 Date birthDate = Context.getPersonService().getPerson(ptId).getBirthdate();
@@ -57,39 +55,42 @@ public class DateMedicallyEligibleForARTCalculation extends AbstractPatientCalcu
                 List<Obs> cd4Percents = CalculationUtils.extractResultValues((ListResult) allCd4Percent.get(ptId));
                 Date arvStartDate = EmrCalculationUtils.datetimeResultForPatient(artStartDateMap, ptId);
 
-                dateAndReason = isEligible(ageInMonths, cd4s, cd4Percents, whoStages, birthDate, arvStartDate);
+                eligibilityInfo = getCriteriaAndDate(ageInMonths, cd4s, cd4Percents, whoStages, birthDate, arvStartDate);
             }
-            ret.put(ptId, new SimpleResult(dateAndReason, this));
+            ret.put(ptId, new SimpleResult(eligibilityInfo, this));
         }
         return ret;
     }
 
-    /**
-     * Checks eligibility based on age, CD4 and WHO stage
-     * @param ageInMonths the patient age in months
-     * @param cd4 the last CD4 count
-     * @param cd4Percent the last CD4 percentage
-     * @param whoStage the last WHO stage
-     * @param birthDate the date when born
-     * @param arvStartDate
-     * @return true if patient is eligible
-     */
-    protected String isEligible(int ageInMonths, List<Obs> cd4, List<Obs> cd4Percent, List<Obs> whoStage, Date birthDate, Date arvStartDate) {
-        String dateAndReason = null;
+	/**
+	 * Checks eligibility based on age, CD4 and WHO stage
+	 * @param ageInMonths
+	 * @param cd4
+	 * @param cd4Percent
+	 * @param whoStage
+	 * @param birthDate
+	 * @param arvStartDate
+	 * @return PatientEligibility information
+	 */
+    protected PatientEligibility getCriteriaAndDate(int ageInMonths, List<Obs> cd4, List<Obs> cd4Percent, List<Obs> whoStage, Date birthDate, Date arvStartDate) {
 
         if (ageInMonths < 24) {
-            dateAndReason = birthDate+"="+"7";
+			return new PatientEligibility("age", birthDate);
+           // dateAndReason = birthDate+"="+"7";
         }
         else if (ageInMonths < 60) { // 24-59 months
             if (whoStage != null && (!cd4.isEmpty())) {
                 for(Obs obsWhoStage:whoStage) {
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_3_PEDS)) {
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+						Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who3Peds", d);
+                        //dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
                     }
 
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_4_PEDS)) {
 
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+                        Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who4Peds", d);
 
                     }
                 }
@@ -98,22 +99,26 @@ public class DateMedicallyEligibleForARTCalculation extends AbstractPatientCalcu
             if (cd4Percent != null && (!cd4Percent.isEmpty())) {
                 for(Obs obsPercent:cd4Percent) {
                     if(obsPercent.getValueNumeric() < 25) {
-                        dateAndReason = (arvStartDate != null && obsPercent.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsPercent.getObsDatetime() + "=" + "2" ;
-                        break;
+						Date d =  (arvStartDate != null && obsPercent.getObsDatetime().after(arvStartDate))? arvStartDate : obsPercent.getObsDatetime() ;
+						return new PatientEligibility("cdPercent", d);
+                        //dateAndReason = (arvStartDate != null && obsPercent.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsPercent.getObsDatetime() + "=" + "2" ;
+
                     }
                 }
             }
             if (cd4 != null && (!cd4.isEmpty())) {
                 for(Obs obsCd4 : cd4) {
                     if(obsCd4.getValueNumeric() < 1000) {
-                        dateAndReason = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsCd4.getObsDatetime() + "=" + "2" ;
-                        break;
+						Date d = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate : obsCd4.getObsDatetime() ;
+						return new PatientEligibility("cd4Count", d);
+                        //dateAndReason = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsCd4.getObsDatetime() + "=" + "2" ;
+
                     }
                 }
             }
         }
         else if (ageInMonths < 155) { // 5-12 years
-			if ( whoStage != null && (!whoStage.isEmpty()) && (!cd4Percent.isEmpty() || !cd4.isEmpty() )) {
+			/*if ( whoStage != null && (!whoStage.isEmpty()) && (!cd4Percent.isEmpty() || !cd4.isEmpty() )) {
 				Map<String, Date> reason = new HashMap<String, Date>();
 				for(Obs obsWhoStage:whoStage) {
 					if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_3_PEDS) || obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_4_PEDS)) {
@@ -189,37 +194,39 @@ public class DateMedicallyEligibleForARTCalculation extends AbstractPatientCalcu
 					}
 				}
 				dateAndReason = iReason;
-			}
+			}*/
             if (whoStage != null && (!whoStage.isEmpty())) {
                 for(Obs obsWhoStage:whoStage) {
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_3_PEDS)) {
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+						Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who3Peds", d);
+                        //dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
                     }
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_4_PEDS)) {
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+                        Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who4Peds", d);
                     }
-                    break;
                 }
             }
             if (cd4Percent != null && (!cd4Percent.isEmpty())) {
                 for(Obs obsPercent : cd4Percent) {
                     if (obsPercent.getValueNumeric() < 20) {
-                        dateAndReason = (arvStartDate != null && obsPercent.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsPercent.getObsDatetime() + "=" + "2" ;
-                        break;
+                        Date d = (arvStartDate != null && obsPercent.getObsDatetime().after(arvStartDate))? arvStartDate : obsPercent.getObsDatetime() ;
+						return new PatientEligibility("cd4Percent", d);
                     }
                 }
             }
             if (cd4 != null && (!cd4.isEmpty())) {
                 for(Obs obsCd4 : cd4) {
                     if (obsCd4.getValueNumeric() < 500) {
-                        dateAndReason = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsCd4.getObsDatetime() + "=" + "2" ;
-                        break;
+						Date d = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate : obsCd4.getObsDatetime() ;
+						return new PatientEligibility("cd4Count", d);
                     }
                 }
             }
         }
         else { // 13+ years
-			if ( whoStage != null && (!whoStage.isEmpty()) && (!cd4Percent.isEmpty() || !cd4.isEmpty() )) {
+			/*if ( whoStage != null && (!whoStage.isEmpty()) && (!cd4Percent.isEmpty() || !cd4.isEmpty() )) {
 				Map<String, Date> reason = new HashMap<String, Date>();
 				for(Obs obsWhoStage:whoStage) {
 					if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_3_PEDS) || obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_4_PEDS)) {
@@ -295,28 +302,29 @@ public class DateMedicallyEligibleForARTCalculation extends AbstractPatientCalcu
 					}
 				}
 				dateAndReason = iReason;
-			}
+			}*/
             if (whoStage != null && (!whoStage.isEmpty())) {
                 for(Obs obsWhoStage:whoStage) {
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_3_ADULT)) {
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+						Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who3Adults", d);
                     }
                     if (obsWhoStage.getValueCoded().equals(Dictionary.WHO_STAGE_4_ADULT)) {
-                        dateAndReason = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsWhoStage.getObsDatetime() + "=" + "1" ;
+						Date d = (arvStartDate != null && obsWhoStage.getObsDatetime().after(arvStartDate))? arvStartDate : obsWhoStage.getObsDatetime();
+						return new PatientEligibility("who4Adults", d);
                     }
-                    break;
                 }
             }
             if (cd4 != null && (!cd4.isEmpty())) {
                 for(Obs obsCd4:cd4) {
                     if(obsCd4.getValueNumeric() < 350) {
-                        dateAndReason = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate + "=" : obsCd4.getObsDatetime() + "=" + "2" ;
-                        break;
+						Date d = (arvStartDate != null && obsCd4.getObsDatetime().after(arvStartDate))? arvStartDate : obsCd4.getObsDatetime() ;
+						return new PatientEligibility("cd4Count", d);
                     }
                 }
             }
         }
-        return dateAndReason;
+        return null;
     }
 
 
--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/MedicallyEligibleConverter.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/MedicallyEligibleConverter.java
@@ -1,13 +1,11 @@
 package org.openmrs.module.kenyaemr.reporting.data.converter;
 
-import org.openmrs.api.context.Context;
 import org.openmrs.calculation.result.CalculationResult;
-import org.openmrs.module.kenyaui.KenyaUiUtils;
+import org.openmrs.module.kenyaemr.reporting.model.PatientEligibility;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -30,24 +28,29 @@ public class MedicallyEligibleConverter implements DataConverter {
 	private String which;
 	@Override
 	public Object convert(Object obj) {
-		KenyaUiUtils kenyaui = Context.getRegisteredComponents(KenyaUiUtils.class).get(0);
 
 		if (obj == null) {
 			return "";
 		}
 
 		Object value = ((CalculationResult) obj).getValue();
+		PatientEligibility eligibility = (PatientEligibility) value;
 
-		if (value instanceof String) {
+			if (eligibility == null) {
+				return null;
+			}
+
+			if (which == null){
+				return null;
+			}
 
 			if(which.equals("date")){
-				return formatDate(stringToDate(((String) value).split("=")[0]));
+				return eligibility.getEligibilityDate() != null ? formatDate(eligibility.getEligibilityDate()) : null;
 			}
 
 			if(which.equals("reason")){
-				return ((String) value).split("=")[1];
+				return eligibility.getCriteria() != null ? eligibility.getCriteria() : null;
 			}
-		}
 
 		return  null;
 	}
@@ -62,15 +65,6 @@ public class MedicallyEligibleConverter implements DataConverter {
 		return String.class;
 	}
 
-	private Date stringToDate(String date) {
-		String datePart = date.split(" ")[0];
-		String[] dateParts = datePart.split("-");
-
-		Calendar calendar = Calendar.getInstance();
-		calendar.set(Integer.parseInt(dateParts[0]), (Integer.parseInt(dateParts[1])-1), Integer.parseInt(dateParts[2]));
-		Date dateRequired = calendar.getTime();
-		return dateRequired;
-	}
 
     private String formatDate(Date date) {
         DateFormat dateFormatter = new SimpleDateFormat("dd/MM/yyyy");
