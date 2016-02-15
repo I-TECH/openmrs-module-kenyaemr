5072f820c7aa3a0a6d50d8b2841a00a0a64be5be
Gitahi Ng'ang'a <gitahi86@gmail.com>
2016-02-15 16:25:31 +0300

    Do not show LMP and EDD in MCH if mother has already delivered.

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/mchms/MchmsEnrollmentSummaryFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/program/mchms/MchmsEnrollmentSummaryFragmentController.java
@@ -1,26 +1,25 @@
 /**
- * The contents of this file are subject to the OpenMRS Public License
- * Version 1.0 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://license.openmrs.org
- *
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- *
+ * The contents of this file are subject to the OpenMRS Public License Version 1.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at http://license.openmrs.org
+ * <p/>
+ * Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either
+ * express or implied. See the License for the specific language governing rights and limitations under the License.
+ * <p/>
  * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
  */
 
 package org.openmrs.module.kenyaemr.fragment.controller.program.mchms;
 
 import org.openmrs.Encounter;
+import org.openmrs.EncounterType;
+import org.openmrs.Form;
 import org.openmrs.Obs;
 import org.openmrs.PatientProgram;
 import org.openmrs.module.kenyacore.CoreUtils;
 import org.openmrs.module.kenyaemr.Dictionary;
-import org.openmrs.module.kenyaemr.util.EmrUtils;
+import org.openmrs.module.kenyaemr.metadata.MchMetadata;
 import org.openmrs.module.kenyaemr.wrapper.Enrollment;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.ui.framework.annotation.FragmentParam;
 import org.openmrs.ui.framework.fragment.FragmentModel;
 
@@ -32,45 +31,53 @@ import java.util.Map;
  */
 public class MchmsEnrollmentSummaryFragmentController {
 
-	public String controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
-							 @FragmentParam(value = "encounter", required = false) Encounter encounter,
-							 @FragmentParam("showClinicalData") boolean showClinicalData,
-							 FragmentModel model) {
+    public String controller(@FragmentParam("patientProgram") PatientProgram patientProgram,
+                             @FragmentParam(value = "encounter", required = false) Encounter encounter,
+                             @FragmentParam("showClinicalData") boolean showClinicalData,
+                             FragmentModel model) {
 
-		Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
-		dataPoints.put("Enrolled", patientProgram.getDateEnrolled());
+        Map<String, Object> dataPoints = new LinkedHashMap<String, Object>();
+        dataPoints.put("Enrolled", patientProgram.getDateEnrolled());
 
-		Enrollment enrollment = new Enrollment(patientProgram);
+        Enrollment enrollment = new Enrollment(patientProgram);
 
-		Obs ancNoObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.ANTENATAL_CASE_NUMBER));
-		if (ancNoObs != null) {
-			dataPoints.put("ANC No", ancNoObs.getValueNumeric().intValue());
-		}
+        Obs ancNoObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.ANTENATAL_CASE_NUMBER));
+        if (ancNoObs != null) {
+            dataPoints.put("ANC No", ancNoObs.getValueNumeric().intValue());
+        }
 
-		Obs lmpObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.LAST_MONTHLY_PERIOD));
-		if (lmpObs != null) {
-			dataPoints.put("LMP", lmpObs.getValueDate());
-			dataPoints.put("EDD (LMP)", CoreUtils.dateAddDays(lmpObs.getValueDate(), 280));
-		}
+        EncounterType mchMsConsultation = MetadataUtils.existing(EncounterType.class, MchMetadata._EncounterType.MCHMS_CONSULTATION);
+        Form delivery = MetadataUtils.existing(Form.class, MchMetadata._Form.MCHMS_DELIVERY);
+        Encounter deliveryEncounter = enrollment.encounterByForm(mchMsConsultation, delivery);
 
-		Obs eddUsoundObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.EXPECTED_DATE_OF_DELIVERY));
-		if (eddUsoundObs != null) {
-			dataPoints.put("EDD (Ultrasound)", eddUsoundObs.getValueDate());
-		}
+        Obs lmpObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.LAST_MONTHLY_PERIOD));
+        if (lmpObs != null) {
+            if (deliveryEncounter == null) {
+                dataPoints.put("LMP", lmpObs.getValueDate());
+                dataPoints.put("EDD (LMP)", CoreUtils.dateAddDays(lmpObs.getValueDate(), 280));
+            }
+        }
 
-		Obs gravidaObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.GRAVIDA));
-		if (gravidaObs != null) {
-			dataPoints.put("Gravida", gravidaObs.getValueNumeric().intValue());
-		}
+        Obs eddUsoundObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.EXPECTED_DATE_OF_DELIVERY));
+        if (eddUsoundObs != null) {
+            if (deliveryEncounter == null) {
+                dataPoints.put("EDD (Ultrasound)", eddUsoundObs.getValueDate());
+            }
+        }
 
-		Obs parityTermObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.PARITY_TERM));
-		Obs parityAbortionObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.PARITY_ABORTION));
+        Obs gravidaObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.GRAVIDA));
+        if (gravidaObs != null) {
+            dataPoints.put("Gravida", gravidaObs.getValueNumeric().intValue());
+        }
 
-		if (parityTermObs != null && parityAbortionObs != null) {
-			dataPoints.put("Parity", parityTermObs.getValueNumeric().intValue() + " + " + parityAbortionObs.getValueNumeric().intValue());
-		}
+        Obs parityTermObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.PARITY_TERM));
+        Obs parityAbortionObs = enrollment.firstObs(Dictionary.getConcept(Dictionary.PARITY_ABORTION));
 
-		model.put("dataPoints", dataPoints);
-		return "view/dataPoints";
-	}
+        if (parityTermObs != null && parityAbortionObs != null) {
+            dataPoints.put("Parity", parityTermObs.getValueNumeric().intValue() + " + " + parityAbortionObs.getValueNumeric().intValue());
+        }
+
+        model.put("dataPoints", dataPoints);
+        return "view/dataPoints";
+    }
 }
