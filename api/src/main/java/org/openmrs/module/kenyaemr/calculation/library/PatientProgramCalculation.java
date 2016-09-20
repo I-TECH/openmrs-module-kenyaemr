d488a4b675070a4fa9bf96fd63b28ed334130f62
werick <jackericmugoma@gmail.com>
2016-09-20 05:40:03 +0300

    Add Regex to UPN and Restrict them from having alphabetical letters and empty spaces Add Calculation to Identify which program a patient belongs to Update lab results form to have provision for ordering testing Add an additional column on the missed appointments list to reflect the program under which the client is enrolled Open the system to allow differentiated model of care where long time appointments are acceptable A way to identify patients whose specimen has been taken for VL is needed and isolate them from the list of those due for viral load

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/PatientProgramCalculation.java
@@ -0,0 +1,75 @@
+package org.openmrs.module.kenyaemr.calculation.library;
+
+import org.openmrs.Patient;
+import org.openmrs.PatientProgram;
+import org.openmrs.Program;
+import org.openmrs.api.context.Context;
+import org.openmrs.calculation.patient.PatientCalculationContext;
+import org.openmrs.calculation.patient.PatientCalculationService;
+import org.openmrs.calculation.result.CalculationResultMap;
+import org.openmrs.calculation.result.SimpleResult;
+import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
+import org.openmrs.module.kenyacore.calculation.CalculationUtils;
+import org.openmrs.module.kenyaemr.Metadata;
+import org.openmrs.module.reporting.cohort.EvaluatedCohort;
+import org.openmrs.module.reporting.cohort.definition.InProgramCohortDefinition;
+
+import java.util.*;
+
+/**
+ * Created by dev on 9/24/16.
+ */
+public class PatientProgramCalculation extends AbstractPatientCalculation {
+    @Override
+    public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> map, PatientCalculationContext context) {
+        CalculationResultMap ret = new CalculationResultMap();
+        InProgramCohortDefinition cd = new InProgramCohortDefinition();
+        List<Program> programs = new ArrayList<Program>();
+        programs.add(Context.getProgramWorkflowService().getProgramByUuid(Metadata.Program.HIV));
+        programs.add(Context.getProgramWorkflowService().getProgramByUuid(Metadata.Program.MCH_CS));
+        programs.add(Context.getProgramWorkflowService().getProgramByUuid(Metadata.Program.MCH_MS));
+        programs.add(Context.getProgramWorkflowService().getProgramByUuid(Metadata.Program.TB));
+        cd.setPrograms(programs);
+
+        EvaluatedCohort patientProgramCohort = CalculationUtils.evaluateWithReporting(cd, cohort, null, context);
+
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("program", programs.get(0));
+        CalculationResultMap hivProgram = new InProgramCalculation().evaluate(cohort, params, Context.getService(PatientCalculationService.class).createCalculationContext());
+
+        params.put("program", programs.get(1));
+        CalculationResultMap mch_csProgram = new InProgramCalculation().evaluate(cohort, params, Context.getService(PatientCalculationService.class).createCalculationContext());
+
+        params.put("program", programs.get(2));
+        CalculationResultMap mch_msProgram = new InProgramCalculation().evaluate(cohort, params, Context.getService(PatientCalculationService.class).createCalculationContext());
+
+        params.put("program", programs.get(3));
+        CalculationResultMap tbProgram = new InProgramCalculation().evaluate(cohort, params, Context.getService(PatientCalculationService.class).createCalculationContext());
+
+
+        for(Integer ptId: cohort){
+            String patientProgram = "";
+            if(patientProgramCohort.contains(ptId)) {
+                if ((Boolean)hivProgram.get(ptId).getValue()) {
+                    patientProgram = "HIV";
+                }
+
+                if ((Boolean)tbProgram.get(ptId).getValue()) {
+                    patientProgram = patientProgram.equalsIgnoreCase("")? "TB" : patientProgram+",TB";
+                }
+
+                if ((Boolean)mch_csProgram.get(ptId).getValue()) {
+                    patientProgram = patientProgram.equalsIgnoreCase("")? "MCH_CS" : patientProgram+",MCH_CS";
+                }
+
+                if ((Boolean)mch_msProgram.get(ptId).getValue()) {
+                    patientProgram = patientProgram.equalsIgnoreCase("")? "MCH_MS" : patientProgram+",MCH_MS";
+                }
+            }
+            ret.put(ptId, new SimpleResult(patientProgram, this));
+        }
+
+        //List<PatientProgram> pp= Context.getProgramWorkflowService().getPatientPrograms(cohort,programs);
+        return ret;
+    }
+}
