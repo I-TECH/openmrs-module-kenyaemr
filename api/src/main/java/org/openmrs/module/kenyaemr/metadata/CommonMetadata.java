fc492a0ff730c9c3fad62a9f997b6e2033cd7f78
ojwanganto <ojwangwachiaje@gmail.com>
2017-08-18 17:38:53 +0300

    renamed hts register to hts form

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/CommonMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/CommonMetadata.java
@@ -116,7 +116,7 @@ public class CommonMetadata extends AbstractMetadataBundle {
 		install(form("Progress Note", "For additional information - mostly complaints and examination findings.", _EncounterType.CONSULTATION, "1", _Form.PROGRESS_NOTE));
 		install(form("Surgical and Medical History", null, _EncounterType.REGISTRATION, "1", _Form.SURGICAL_AND_MEDICAL_HISTORY));
 		install(form("Triage", null, _EncounterType.TRIAGE, "1", _Form.TRIAGE));
-		install(form("HTS Register", "Register for HTS Services", _EncounterType.HTS, "1", _Form.HTS_REGISTER));
+		install(form("HTS Form", "Form for HTS Services", _EncounterType.HTS, "1", _Form.HTS_REGISTER));
 
 
 		install(globalProperty(EmrConstants.GP_DEFAULT_LOCATION, "The facility for which this installation is configured",
