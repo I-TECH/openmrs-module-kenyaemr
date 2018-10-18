e170d7e40f314e989bc6caf29de0dd80ef3891a8
njorocs <njorocs@gmail.com>
2018-10-18 17:33:01 +0300

    Addded evaluators for Maternity register and completed data definitions

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/maternity/MaternityCommentsDataEvaluator.java
@@ -0,0 +1,37 @@
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator.maternity;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.maternity.MaternityARVProphylaxisIssuedAtMaternityDataDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.maternity.MaternityCommentsDataDefinition;
+import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
+import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
+import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates a PersonDataDefinition
+ */
+@Handler(supports= MaternityCommentsDataDefinition.class, order=50)
+public class MaternityCommentsDataEvaluator implements PersonDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
+
+        String qry = "";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
