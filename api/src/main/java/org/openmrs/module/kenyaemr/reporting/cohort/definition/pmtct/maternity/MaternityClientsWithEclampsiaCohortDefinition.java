0b3b87cc0d79658e591203dc1ddb37f3f2aa7a13
njorocs <njorocs@gmail.com>
2018-11-02 17:44:22 +0300

    Added Cohort indicators for Maternity

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/cohort/definition/pmtct/maternity/MaternityClientsWithEclampsiaCohortDefinition.java
@@ -0,0 +1,33 @@
+package org.openmrs.module.kenyaemr.reporting.cohort.definition.pmtct.maternity;
+
+import org.openmrs.Encounter;
+import org.openmrs.module.reporting.common.Localized;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyCachingStrategy;
+import org.openmrs.module.reporting.evaluation.caching.Caching;
+import org.openmrs.module.reporting.query.BaseQuery;
+import org.openmrs.module.reporting.query.encounter.definition.EncounterQuery;
+
+import java.util.Date;
+
+/**
+ * HTS Register cohort definition
+ */
+@Caching(strategy = ConfigurationPropertyCachingStrategy.class)
+@Localized("reporting.ANCRegisterCohortDefinition")
+public class MaternityClientsWithEclampsiaCohortDefinition extends BaseQuery<Encounter> implements EncounterQuery {
+
+    @ConfigurationProperty
+    private Date asOfDate;
+
+    public MaternityClientsWithEclampsiaCohortDefinition() {
+    }
+
+    public Date getAsOfDate() {
+        return asOfDate;
+    }
+
+    public void setAsOfDate(Date asOfDate) {
+        this.asOfDate = asOfDate;
+    }
+}
