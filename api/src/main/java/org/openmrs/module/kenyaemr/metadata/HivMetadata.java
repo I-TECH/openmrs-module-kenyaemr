b719a5f2b0ca737e9a28774ae7c6623c847c87f3
Patrick <gpatryllus@gmail.com>
2019-02-04 16:50:23 +0300

    Added alcohol and drug abuse screening form,gender based violence form and art treatme t preparation form

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/HivMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/HivMetadata.java
@@ -56,6 +56,8 @@ public class HivMetadata extends AbstractMetadataBundle {
 		public static final String FAMILY_TESTING_FORM_FOR_NEGATIVE_CLIENTS = "62846fae-8d0b-4202-827e-8b6ffd30e587";
 		public static final String DRUG_ORDER = "888dbabd-1c18-4653-82c2-e753415ab79a";
 		public static final String TREATMENT_PREPARATION = "782a4263-3ac9-4ce8-b316-534571233f12";
+		public static final String GBV_SCREENING = "03767614-1384-4ce3-aea9-27e2f4e67d01";
+		public static final String ALCOHOL_AND_DRUGS_SCREENING = "7b1ec2d5-a4ad-4ffc-a0d3-ff1ea68e293c";
 
 	}
 
@@ -94,6 +96,8 @@ public class HivMetadata extends AbstractMetadataBundle {
 		install(form("Family and Partner Testing Results", "Family and Partner Testing for HIV Negative Patients", _EncounterType.FAMILY_AND_PARTNER_TESTING, "1", _Form.FAMILY_TESTING_FORM_FOR_NEGATIVE_CLIENTS));
 		install(form("Drug Order", "Drug Order", _EncounterType.DRUG_ORDER, "1", _Form.DRUG_ORDER));
 		install(form("ART Preparation", "ART Preparation", _EncounterType.HIV_CONSULTATION, "1", _Form.TREATMENT_PREPARATION));
+		install(form("Gender Based Violence Screening", "Gender Based Violence Screening", _EncounterType.HIV_CONSULTATION, "1", _Form.GBV_SCREENING));
+		install(form("Alcohol and Drug Abuse Screening", "Alcohol and Drug Abuse Screening", _EncounterType.HIV_CONSULTATION, "1", _Form.ALCOHOL_AND_DRUGS_SCREENING));
 
 		install(patientIdentifierType("Unique Patient Number", "Assigned to every HIV patient", "^[0-9]{10,11}$", "Facility code followed by sequential number",
 				null, LocationBehavior.NOT_USED, false, _PatientIdentifierType.UNIQUE_PATIENT_NUMBER));
