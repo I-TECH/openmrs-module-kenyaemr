1a31de6ebe5ba09aaafd16f5a9ea3ecb2aae329b
ojwanganto <ojwangwachiaje@gmail.com>
2019-02-22 17:35:49 +0300

    added defaulter tracing register

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/defaulterTracing/FinalOutcomeDataEvaluator.java
@@ -0,0 +1,45 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator.defaulterTracing;
+
+import org.openmrs.annotation.Handler;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.defaulterTracing.FinalOutcomeDataDefinition;
+import org.openmrs.module.reporting.data.encounter.EvaluatedEncounterData;
+import org.openmrs.module.reporting.data.encounter.definition.EncounterDataDefinition;
+import org.openmrs.module.reporting.data.encounter.evaluator.EncounterDataEvaluator;
+import org.openmrs.module.reporting.evaluation.EvaluationContext;
+import org.openmrs.module.reporting.evaluation.EvaluationException;
+import org.openmrs.module.reporting.evaluation.querybuilder.SqlQueryBuilder;
+import org.openmrs.module.reporting.evaluation.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+
+/**
+ * Evaluates final tracing outcome to produce a VisitData
+ */
+@Handler(supports=FinalOutcomeDataDefinition.class, order=50)
+public class FinalOutcomeDataEvaluator implements EncounterDataEvaluator {
+
+    @Autowired
+    private EvaluationService evaluationService;
+
+    public EvaluatedEncounterData evaluate(EncounterDataDefinition definition, EvaluationContext context) throws EvaluationException {
+        EvaluatedEncounterData c = new EvaluatedEncounterData(definition, context);
+
+        String qry = "select encounter_id, (case true_status  when 160432 then 'Dead' when 1693 then 'Receiving ART from another clinic/Transferred' when 160037 then 'Still in care at CCC' when 5240 then 'Lost to follow up' when 142917 then 'Other' else ''  end) as outcome from kenyaemr_etl.etl_ccc_defaulter_tracing ";
+
+        SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
+        queryBuilder.append(qry);
+        Map<Integer, Object> data = evaluationService.evaluateToMap(queryBuilder, Integer.class, Object.class, context);
+        c.setData(data);
+        return c;
+    }
+}
