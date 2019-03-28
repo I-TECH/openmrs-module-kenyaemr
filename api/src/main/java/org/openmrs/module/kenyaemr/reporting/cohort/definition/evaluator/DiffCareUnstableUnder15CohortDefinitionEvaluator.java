8ec900d0f05e029eb89894b9503860e17e3a7935
njorocs <njorocs@gmail.com>
2019-03-28 16:51:33 +0300

    Revised differentiated care  queries to reconcile figures for current in care on ART and stability disaggregates. Added undocumented stability indicator in Diff care dashboard and simple report. Added grouping categories in Contact testing highchart.

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/cohort/definition/evaluator/DiffCareUnstableUnder15CohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/cohort/definition/evaluator/DiffCareUnstableUnder15CohortDefinitionEvaluator.java
@@ -47,10 +47,13 @@ public class DiffCareUnstableUnder15CohortDefinitionEvaluator implements CohortD
 
 		Cohort newCohort = new Cohort();
 
-		String qry="select c.patient_id from kenyaemr_etl.etl_current_in_care c  inner join kenyaemr_etl.etl_patient_hiv_followup f\n" +
-				"                             on c.patient_id = f.patient_id\n" +
-				"   inner join kenyaemr_etl.etl_patient_demographics d on d.patient_id = c.patient_id and timestampdiff(year ,d.dob,c.latest_vis_date)< 15\n" +
-				"   where f.stability = 2 and c.started_on_drugs is not null group by c.patient_id;";
+		String qry="select patient_id from(\n" +
+				"                      select c.patient_id,f.stability stability,c.latest_vis_date latest_visit_date,f.visit_date fup_visit_date,\n" +
+				"                             c.dob dob\n" +
+				"                      from kenyaemr_etl.etl_current_in_care c\n" +
+				"                             inner join kenyaemr_etl.etl_patient_hiv_followup f on f.patient_id = c.patient_id and c.latest_vis_date =f.visit_date\n" +
+				"                      where c.started_on_drugs is not null  and f.voided = 0 group by c.patient_id) cic where cic.stability=2\n" +
+				"                                                                                                          and timestampdiff(year ,cic.dob,cic.latest_visit_date) <15;";
 
 		SqlQueryBuilder builder = new SqlQueryBuilder();
 		builder.append(qry);
