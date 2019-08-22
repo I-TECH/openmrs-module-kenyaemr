1c091b9f0363d4f52ea48fe264d212e14b87ba10
ojwanganto <ojwangwachiaje@gmail.com>
2019-08-22 18:10:00 +0300

    add test columns

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/hts/EnrollmentTrackerReportBuilder.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/builder/hts/EnrollmentTrackerReportBuilder.java
@@ -15,22 +15,30 @@ import org.openmrs.module.kenyacore.report.ReportUtils;
 import org.openmrs.module.kenyacore.report.builder.AbstractHybridReportBuilder;
 import org.openmrs.module.kenyacore.report.builder.Builds;
 import org.openmrs.module.kenyacore.report.data.patient.definition.CalculationDataDefinition;
+import org.openmrs.module.kenyaemr.Dictionary;
 import org.openmrs.module.kenyaemr.calculation.library.hiv.art.DateOfEnrollmentArtCalculation;
 import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.openmrs.module.kenyaemr.reporting.calculation.converter.DateArtStartDateConverter;
-import org.openmrs.module.kenyaemr.reporting.calculation.converter.GenderConverter;
+import org.openmrs.module.kenyaemr.reporting.cohort.definition.EnrollmentTrackerCohortDefinition;
 import org.openmrs.module.kenyaemr.reporting.cohort.definition.RDQACohortDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.enrollmentTracker.HtsDateConfirmedPositiveDataDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.enrollmentTracker.HtsSameDayEnrollmentDataDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.enrollmentTracker.HtsSettingDataDefinition;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.module.reporting.cohort.definition.CohortDefinition;
+import org.openmrs.module.reporting.common.TimeQualifier;
 import org.openmrs.module.reporting.data.DataDefinition;
 import org.openmrs.module.reporting.data.converter.BirthdateConverter;
 import org.openmrs.module.reporting.data.converter.DataConverter;
 import org.openmrs.module.reporting.data.converter.ObjectFormatter;
+import org.openmrs.module.reporting.data.converter.ObsValueConverter;
 import org.openmrs.module.reporting.data.patient.definition.ConvertedPatientDataDefinition;
 import org.openmrs.module.reporting.data.patient.definition.PatientIdentifierDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.AgeDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.BirthdateDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.ConvertedPersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.GenderDataDefinition;
+import org.openmrs.module.reporting.data.person.definition.ObsForPersonDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PersonIdDataDefinition;
 import org.openmrs.module.reporting.data.person.definition.PreferredNameDataDefinition;
 import org.openmrs.module.reporting.dataset.definition.PatientDataSetDefinition;
@@ -59,16 +67,22 @@ public class EnrollmentTrackerReportBuilder extends AbstractHybridReportBuilder 
         dsd.addColumn("id", new PersonIdDataDefinition(), "");
         dsd.addColumn("Name", nameDef, "");
         dsd.addColumn("Unique Patient No", identifierDef, "");
-        dsd.addColumn("Sex", new GenderDataDefinition(), "", new GenderConverter());
+        dsd.addColumn("Sex", new GenderDataDefinition(), "", null);
+        dsd.addColumn("Age", new AgeDataDefinition(), "", null);
         dsd.addColumn("Date of Birth", new BirthdateDataDefinition(), "", new BirthdateConverter(DATE_FORMAT));
         dsd.addColumn("Enrollment Date", new CalculationDataDefinition("Enrollment Date", new DateOfEnrollmentArtCalculation()), "", new DateArtStartDateConverter());
+        dsd.addColumn("Occupation", new ObsForPersonDataDefinition("Occupation", TimeQualifier.LAST, Dictionary.getConcept(Dictionary.OCCUPATION), null, null), "", new ObsValueConverter());
+        dsd.addColumn("Setting", new HtsSettingDataDefinition(), "", null);
+        dsd.addColumn("Date Confirmed Positive", new HtsDateConfirmedPositiveDataDefinition(), "", null);
+        dsd.addColumn("Enrolled on same day", new HtsSameDayEnrollmentDataDefinition(), "", null);
 
     }
 
     @Override
     protected Mapped<CohortDefinition> buildCohort(HybridReportDescriptor descriptor, PatientDataSetDefinition dsd) {
-        CohortDefinition cd = new RDQACohortDefinition();
-        cd.setName("RDQA All Patients");
+        CohortDefinition cd = new EnrollmentTrackerCohortDefinition();
+        cd.setName("Patients tested and enrolled");
         return ReportUtils.map(cd, "");
+
     }
 }
