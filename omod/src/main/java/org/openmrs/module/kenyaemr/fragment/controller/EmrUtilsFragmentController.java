4643e9474ca25f0227bf2dd5ce288e25b3a407d9
ojwanganto <ojwangwachiaje@gmail.com>
2019-08-23 17:36:12 +0300

    add dialog for starting ipt in green card

--- omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/EmrUtilsFragmentController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/fragment/controller/EmrUtilsFragmentController.java
@@ -12,11 +12,20 @@ package org.openmrs.module.kenyaemr.fragment.controller;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Location;
+import org.openmrs.Obs;
 import org.openmrs.Patient;
 import org.openmrs.PatientIdentifierType;
+import org.openmrs.PatientProgram;
 import org.openmrs.Relationship;
 import org.openmrs.Visit;
+import org.openmrs.VisitType;
+import org.openmrs.api.APIAuthenticationException;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.EncounterService;
 import org.openmrs.api.PatientService;
+import org.openmrs.api.ProgramWorkflowService;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.ContextAuthenticationException;
 import org.openmrs.calculation.result.CalculationResult;
@@ -25,10 +34,13 @@ import org.openmrs.module.kenyaemr.Metadata;
 import org.openmrs.module.kenyaemr.api.KenyaEmrService;
 import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
 import org.openmrs.module.kenyaemr.calculation.library.hiv.art.InitialArtStartDateCalculation;
+import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
+import org.openmrs.module.kenyaemr.metadata.IPTMetadata;
 import org.openmrs.module.kenyaemr.regimen.RegimenChange;
 import org.openmrs.module.kenyaemr.regimen.RegimenChangeHistory;
 import org.openmrs.module.kenyaemr.regimen.RegimenManager;
 import org.openmrs.module.kenyaemr.util.EmrUiUtils;
+import org.openmrs.module.kenyaemr.util.EmrUtils;
 import org.openmrs.module.kenyaui.KenyaUiUtils;
 import org.openmrs.module.kenyaui.annotation.AppAction;
 import org.openmrs.module.kenyaui.annotation.PublicAction;
@@ -290,5 +302,242 @@ public class EmrUtilsFragmentController {
 		cal.setTime(now != null ? now : new Date());
 		cal.add(Calendar.YEAR, -age);
 		return SimpleObject.create("birthdate", kenyaui.formatDateParam(cal.getTime()));
+	}
+
+	/**
+	 * enrolls a patient in IPT program
+	 * @param patient to be enrolled
+	 * @param enrollmentDate date to be enrolled
+	 * @return simple object with patient program id
+	 */
+	public SimpleObject enrollInIptProgram(@RequestParam("patientId") Patient patient, @RequestParam("enrollmentDate") Date enrollmentDate) {
+		PatientProgram iptEnrollment = new PatientProgram();
+		ProgramWorkflowService programWorkflowService = Context.getProgramWorkflowService();
+		iptEnrollment.setProgram(programWorkflowService.getProgramByUuid(IPTMetadata._Program.IPT));
+		iptEnrollment.setPatient(patient);
+		iptEnrollment.setDateEnrolled(enrollmentDate);
+		iptEnrollment.setLocation(Context.getService(KenyaEmrService.class).getDefaultLocation());
+		PatientProgram enrolled = programWorkflowService.savePatientProgram(iptEnrollment);
+
+		// add ipt initiation encounter
+		Encounter enc = new Encounter();
+		enc.setLocation(Context.getService(KenyaEmrService.class).getDefaultLocation());
+		EncounterService encounterService = Context.getEncounterService();
+		enc.setEncounterType(encounterService.getEncounterTypeByUuid(IPTMetadata._EncounterType.IPT_INITIATION));
+		enc.setEncounterDatetime(enrollmentDate);
+		enc.setPatient(patient);
+		enc.addProvider(encounterService.getEncounterRole(1), Context.getProviderService().getProvider(1));
+		enc.setForm(Context.getFormService().getFormByUuid(IPTMetadata._Form.IPT_INITIATION));
+		encounterService.saveEncounter(enc);
+
+		return SimpleObject.create("enrolledInIpt", enrolled != null ? enrolled.getPatientProgramId() : null);
+
+	}
+
+	/**
+	 * completes IPT program
+	 * @param patient
+	 * @param event i.e. discontinued, ltfu, completed, etc
+	 * @param completionDate
+	 * @return
+	 */
+	public SimpleObject discontinueIptProgram(@RequestParam("patientId") Patient patient,
+										   @RequestParam("event") Concept event,
+										   @RequestParam("reason") Concept reason,
+										   @RequestParam("action") String action,
+										   @RequestParam("completionDate") Date completionDate) {
+		ProgramWorkflowService programWorkflowService = Context.getProgramWorkflowService();
+		List<PatientProgram> iptProgramEnrollments = programWorkflowService.getPatientPrograms(patient, programWorkflowService.getProgramByUuid(IPTMetadata._Program.IPT), null, null, null, null, false );
+		ConceptService conceptService = Context.getConceptService();
+		String discontinuationEventConcept = "160433AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		String discontinuationReasonConcept = "1266AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+		String actionTakenConcept = "160632AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+		boolean successful = false;
+
+		PatientProgram lastEnrollment = null;
+		if (iptProgramEnrollments != null && iptProgramEnrollments.size() > 0) {
+			lastEnrollment = iptProgramEnrollments.get(0);
+		}
+		if (lastEnrollment != null) {
+			lastEnrollment.setDateCompleted(completionDate);
+			lastEnrollment.setOutcome(event);
+			programWorkflowService.savePatientProgram(lastEnrollment);
+
+			// add outcome encounter
+
+			Encounter enc = new Encounter();
+			enc.setLocation(Context.getService(KenyaEmrService.class).getDefaultLocation());
+			EncounterService encounterService = Context.getEncounterService();
+			enc.setEncounterType(encounterService.getEncounterTypeByUuid(IPTMetadata._EncounterType.IPT_OUTCOME));
+			enc.setEncounterDatetime(completionDate);
+			enc.setPatient(patient);
+			enc.addProvider(encounterService.getEncounterRole(1), Context.getProviderService().getProvider(1));
+			enc.setForm(Context.getFormService().getFormByUuid(IPTMetadata._Form.IPT_OUTCOME));
+
+			// build discontinuation observations
+			if (event != null) {
+				Obs o = new Obs(); // build event obs
+				o.setConcept(conceptService.getConcept(discontinuationEventConcept));
+				o.setDateCreated(new Date());
+				o.setCreator(Context.getAuthenticatedUser());
+				o.setLocation(enc.getLocation());
+				o.setObsDatetime(enc.getEncounterDatetime());
+				o.setPerson(patient);
+				o.setValueCoded(event);
+				enc.addObs(o);
+			}
+			if (reason != null) {
+				Obs discReason = new Obs(); // build reason obs
+				discReason.setConcept(conceptService.getConcept(discontinuationReasonConcept));
+				discReason.setDateCreated(new Date());
+				discReason.setCreator(Context.getAuthenticatedUser());
+				discReason.setLocation(enc.getLocation());
+				discReason.setObsDatetime(enc.getEncounterDatetime());
+				discReason.setPerson(patient);
+				discReason.setValueCoded(reason);
+				enc.addObs(discReason);
+			}
+
+			if (action != null) {
+				Obs discAction = new Obs(); // build reason obs
+				discAction.setConcept(conceptService.getConcept(actionTakenConcept));
+				discAction.setDateCreated(new Date());
+				discAction.setCreator(Context.getAuthenticatedUser());
+				discAction.setLocation(enc.getLocation());
+				discAction.setObsDatetime(enc.getEncounterDatetime());
+				discAction.setPerson(patient);
+				discAction.setValueText(action);
+				enc.addObs(discAction);
+			}
+			Encounter discEnc = encounterService.saveEncounter(enc);
+			successful = discEnc.getEncounterId() != null ? true : false;
+
+		}
+		return SimpleObject.create("discontinueInIpt", successful ? lastEnrollment.getPatientProgramId() : null);
+
+	}
+
+	public SimpleObject addIptFollowup(@RequestParam("patientId") Patient patient, @RequestParam("outcomeConcept") Concept outcome, @RequestParam("encounterDate") Date encounterDate) {
+		Encounter enc = new Encounter();
+		enc.setLocation(Context.getService(KenyaEmrService.class).getDefaultLocation());
+		EncounterService encounterService = Context.getEncounterService();
+		enc.setEncounterType(encounterService.getEncounterTypeByUuid(IPTMetadata._EncounterType.IPT_FOLLOWUP));
+		enc.setEncounterDatetime(encounterDate);
+		enc.setPatient(patient);
+		enc.addProvider(encounterService.getEncounterRole(1), Context.getProviderService().getProvider(1));
+		enc.setForm(Context.getFormService().getFormByUuid(IPTMetadata._Form.IPT_FOLLOWUP));
+		Visit v = getActiveVisit(patient);
+		if (v != null) {
+			enc.setVisit(v);
+			encounterService.saveEncounter(enc);
+			return SimpleObject.create("enrolledInIpt", enc != null ? enc.getEncounterId() : null);
+		}
+
+		return SimpleObject.create("enrolledInIpt", "There was a problem adding IPT follow-up details");
+
+	}
+
+	/**
+	 * Gets the active visit for the given patient
+	 * @param patient the patient
+	 * @return the active visit
+	 */
+	protected Visit getActiveVisit(Patient patient) {
+		try {
+			List<Visit> activeVisits = Context.getVisitService().getActiveVisitsByPatient(patient);
+			if (activeVisits.size() > 0) {
+				return activeVisits.get(0);
+			} else {
+				Visit newVisit = new Visit();
+				newVisit.setPatient(patient);
+				newVisit.setStartDatetime(new Date());
+				newVisit.setVisitType(MetadataUtils.existing(VisitType.class, CommonMetadata._VisitType.OUTPATIENT));
+				return Context.getVisitService().saveVisit(newVisit);
+			}
+		}
+		catch (APIAuthenticationException ex) {
+			return null; // Swallow API authentication exceptions
+		}
+	}
+
+	private void setEncounterObs(Encounter enc) {
+
+		Integer finalHivTestResultConcept = 159427;
+		Integer testTypeConcept = 162084;
+		Integer testStrategyConcept = 164956;
+		Integer healthProviderConcept = 1473;
+		Integer healthFacilityNameConcept = 162724;
+		Integer healthProviderIdentifierConcept = 163161;
+		// test result
+		/*Obs o = new Obs();
+		o.setConcept(conceptService.getConcept(finalHivTestResultConcept));
+		o.setDateCreated(new Date());
+		o.setCreator(Context.getUserService().getUser(1));
+		o.setLocation(enc.getLocation());
+		o.setObsDatetime(enc.getEncounterDatetime());
+		o.setPerson(this.patient);
+		o.setValueCoded(hivTest.getResult());
+
+		// test type
+		Obs o1 = new Obs();
+		o1.setConcept(conceptService.getConcept(testTypeConcept));
+		o1.setDateCreated(new Date());
+		o1.setCreator(Context.getUserService().getUser(1));
+		o1.setLocation(enc.getLocation());
+		o1.setObsDatetime(enc.getEncounterDatetime());
+		o1.setPerson(this.patient);
+		o1.setValueCoded(testTypeConverter(hivTest.getType().trim()));
+
+		// test strategy
+		Obs o2 = new Obs();
+		o2.setConcept(conceptService.getConcept(testStrategyConcept));
+		o2.setDateCreated(new Date());
+		o2.setCreator(Context.getUserService().getUser(1));
+		o2.setLocation(enc.getLocation());
+		o2.setObsDatetime(enc.getEncounterDatetime());
+		o2.setPerson(this.patient);
+		o2.setValueCoded(hivTest.getStrategy());
+
+		// test provider
+		// only do this if provider details is not null
+
+		Obs o3 = new Obs();
+		o3.setConcept(conceptService.getConcept(healthProviderConcept));
+		o3.setDateCreated(new Date());
+		o3.setCreator(Context.getUserService().getUser(1));
+		o3.setLocation(enc.getLocation());
+		o3.setObsDatetime(enc.getEncounterDatetime());
+		o3.setPerson(this.patient);
+		o3.setValueText(hivTest.getProviderName().trim());
+
+		// test provider id
+		Obs o5 = new Obs();
+		o5.setConcept(conceptService.getConcept(healthProviderIdentifierConcept));
+		o5.setDateCreated(new Date());
+		o5.setCreator(Context.getUserService().getUser(1));
+		o5.setLocation(enc.getLocation());
+		o5.setObsDatetime(enc.getEncounterDatetime());
+		o5.setPerson(this.patient);
+		o5.setValueText(hivTest.getProviderId().trim());
+
+		// test facility
+		Obs o4 = new Obs();
+		o4.setConcept(conceptService.getConcept(healthFacilityNameConcept));
+		o4.setDateCreated(new Date());
+		o4.setCreator(Context.getUserService().getUser(1));
+		o4.setLocation(enc.getLocation());
+		o4.setObsDatetime(enc.getEncounterDatetime());
+		o4.setPerson(this.patient);
+		o4.setValueText(hivTest.getFacility().trim());
+
+
+		enc.addObs(o);
+		enc.addObs(o1);
+		enc.addObs(o2);
+		enc.addObs(o3);
+		enc.addObs(o4);
+		enc.addObs(o5);
+		encounterService.saveEncounter(enc);*/
 	}
 }
