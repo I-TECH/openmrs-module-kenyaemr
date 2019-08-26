7578c3a2501164d165a0a5c593229d8eea62dcf6
ojwanganto <ojwangwachiaje@gmail.com>
2019-08-27 00:09:45 +0300

    cleaned up On Ipt calculation

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/ipt/OnIptProgramCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/ipt/OnIptProgramCalculation.java
@@ -11,25 +11,18 @@ package org.openmrs.module.kenyaemr.calculation.library.ipt;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.joda.time.DateTime;
-import org.joda.time.Minutes;
-import org.openmrs.Concept;
-import org.openmrs.Obs;
 import org.openmrs.Program;
-import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
 import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
 import org.openmrs.module.kenyacore.calculation.BooleanResult;
-import org.openmrs.module.kenyacore.calculation.Calculations;
 import org.openmrs.module.kenyacore.calculation.Filters;
 import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
-import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
+import org.openmrs.module.kenyaemr.metadata.IPTMetadata;
 import org.openmrs.module.kenyaemr.metadata.TbMetadata;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 
 import java.util.Collection;
-import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
@@ -37,6 +30,7 @@ import java.util.Set;
  * Calculates whether patients are (alive and) in the IPT program
  * Eligibility criteria include:
  * Is currently active in IPT program
+ * should not include those currently in the TB program
  *
  */
 public class OnIptProgramCalculation extends AbstractPatientCalculation implements PatientFlagCalculation {
@@ -46,68 +40,23 @@ public class OnIptProgramCalculation extends AbstractPatientCalculation implemen
 	@Override
 	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
 
-		Concept IptCurrentQuestion = Context.getConceptService().getConcept(164949);
-		Concept IptStartQuestion = Context.getConceptService().getConcept(1265);
-		Concept IptStopQuestion = Context.getConceptService().getConcept(160433);
-
 		Program tbProgram = MetadataUtils.existing(Program.class, TbMetadata._Program.TB);
+		Program iptProgram = MetadataUtils.existing(Program.class, IPTMetadata._Program.IPT);
 		Set<Integer> alive = Filters.alive(cohort, context);
 		Set<Integer> inTbProgram = Filters.inProgram(tbProgram, alive, context);
-
-		CalculationResultMap iptCurrent = Calculations.lastObs(IptCurrentQuestion, cohort, context);
-		CalculationResultMap iptStarted = Calculations.lastObs(IptStartQuestion, cohort, context);
-		CalculationResultMap iptStopped = Calculations.lastObs(IptStopQuestion, cohort, context);
+		Set<Integer> inIptProgram = Filters.inProgram(iptProgram, alive, context);
 
 		CalculationResultMap ret = new CalculationResultMap();
 		for(Integer ptId: cohort){
 
-			Date iptStartObsDate = null;
-			Date iptStopObsDate = null;
-			boolean tbPatient = false;
-			boolean inIptProgram = false;
-			boolean currentInIPT = false;
-			boolean patientInTbProgram = false;
-			Integer iptStartStopDiff = 0;
-
-			Obs iptCurrentObs = EmrCalculationUtils.obsResultForPatient(iptCurrent, ptId);
-			Obs iptStartObs = EmrCalculationUtils.obsResultForPatient(iptStarted, ptId);
-			Obs iptStopObs = EmrCalculationUtils.obsResultForPatient(iptStopped, ptId);
-
-			//Eligibility not for patients already in tb program
-			if (inTbProgram.contains(ptId)) {
-				patientInTbProgram = true;
+			boolean onIpt = false;
+			if (!inTbProgram.contains(ptId) && inIptProgram.contains(ptId)) {
+				onIpt = true;
 			}
 
-			//Currently on IPT
-			if (iptCurrentObs != null &&  iptStopObs == null && iptCurrentObs.getValueCoded().getConceptId().equals(1065)) {
-				inIptProgram = true;
-			}
-			//Started on IPT
-			if (iptStartObs != null &&  iptStopObs == null && iptStartObs.getValueCoded().getConceptId().equals(1065)) {
-				inIptProgram = true;
-			}
-			//Repeat on IPT
-			if(iptStartObs != null && iptStopObs != null && iptStartObs.getValueCoded().getConceptId().equals(1065)) {
-				iptStartObsDate = iptStartObs.getObsDatetime();
-				iptStopObsDate = iptStopObs.getObsDatetime();
-				iptStartStopDiff = minutesBetween(iptStopObsDate,iptStartObsDate);
-				if (iptStartStopDiff > 1) {
-					inIptProgram = true;
-				}
-			}
-
-			if (!patientInTbProgram && inIptProgram)
-				currentInIPT = true;
-
-			ret.put(ptId, new BooleanResult(currentInIPT, this));
+			ret.put(ptId, new BooleanResult(onIpt, this));
 		}
 		return ret;
-	}
-	private int minutesBetween(Date date1, Date date2) {
-		DateTime d1 = new DateTime(date1.getTime());
-		DateTime d2 = new DateTime(date2.getTime());
-		return Minutes.minutesBetween(d1, d2).getMinutes();
-
 	}
 
 	@Override
