6b01912125da11b6c76ae7acd9f1fa198706e02c
njorocs <njorocs@gmail.com>
2019-09-19 12:15:11 +0300

    Added more Queries for IPT Register

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/ipt/TreatmentStartDateDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/ipt/TreatmentStartDateDataEvaluator.java
@@ -35,8 +35,8 @@ public class TreatmentStartDateDataEvaluator implements PersonDataEvaluator {
         EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
 
         String qry = "select init.patient_id, date(o.date_activated) treatment_start_date from kenyaemr_etl.etl_ipt_initiation init left outer join openmrs.orders o on init.patient_id = o.patient_id\n" +
-                "                                                                          left join  openmrs.drug_order do on o.order_number = do.order_id\n" +
-                "where do.drug_inventory_id in (1800,1801,1802,1803,1804,1805,1806,1807,1808) and o.order_type_id = 2 group by init.patient_id having min(do.order_id);";
+                "                                                                                                              left join  openmrs.drug_order do on o.order_number = do.order_id\n" +
+                "where do.drug_inventory_id in (1800,1801,1802,1803,1804,1805,1806,1807,1808) and o.order_type_id = 2 group by init.patient_id having mid(min(concat(o.date_activated,o.order_id)),11);";
 
         SqlQueryBuilder queryBuilder = new SqlQueryBuilder();
         queryBuilder.append(qry);
