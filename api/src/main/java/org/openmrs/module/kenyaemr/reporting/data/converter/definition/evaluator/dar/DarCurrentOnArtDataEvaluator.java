a971aaaf520a86c74d224ac7fc8dba314bba1025
ojwanganto <ojwangwachiaje@gmail.com>
2019-09-19 13:44:24 +0300

    define and add current on art data definition to the dar register

--- api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/dar/DarCurrentOnArtDataEvaluator.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/data/converter/definition/evaluator/dar/DarCurrentOnArtDataEvaluator.java
@@ -10,7 +10,7 @@
 package org.openmrs.module.kenyaemr.reporting.data.converter.definition.evaluator.dar;
 
 import org.openmrs.annotation.Handler;
-import org.openmrs.module.kenyaemr.reporting.data.converter.definition.dar.DarStartingArtDataDefinition;
+import org.openmrs.module.kenyaemr.reporting.data.converter.definition.dar.DarCurrentOnArtDataDefinition;
 import org.openmrs.module.reporting.data.person.EvaluatedPersonData;
 import org.openmrs.module.reporting.data.person.definition.PersonDataDefinition;
 import org.openmrs.module.reporting.data.person.evaluator.PersonDataEvaluator;
@@ -26,33 +26,35 @@ import java.util.Map;
 /**
  * Evaluates DarStartingArtDataDefinition to produce a VisitData
  */
-@Handler(supports= DarStartingArtDataDefinition.class, order=50)
-public class DarStartingArtDataEvaluator implements PersonDataEvaluator {
+@Handler(supports= DarCurrentOnArtDataDefinition.class, order=50)
+public class DarCurrentOnArtDataEvaluator implements PersonDataEvaluator {
 
     @Autowired
     private EvaluationService evaluationService;
 
     public EvaluatedPersonData evaluate(PersonDataDefinition definition, EvaluationContext context) throws EvaluationException {
         EvaluatedPersonData c = new EvaluatedPersonData(definition, context);
-        DarStartingArtDataDefinition def = (DarStartingArtDataDefinition) definition;
+        DarCurrentOnArtDataDefinition def = (DarCurrentOnArtDataDefinition) definition;
         Integer minAge = def.getMinAge();
         Integer maxAge = def.getMaxAge();
         String sex = def.getSex();
         String qry = "";
         if (sex != null) {
-            qry = "SELECT e.patient_id, 'X' hasFollowupVisit\n" +
-                    "FROM kenyaemr_etl.etl_hiv_enrollment e\n" +
-                    "         inner join kenyaemr_etl.etl_patient_demographics p on p.patient_id = e.patient_id and  p.voided = 0 and p.Gender = ':sex' \n" +
-                    "         inner join kenyaemr_etl.etl_patient_hiv_followup f on f.patient_id = e.patient_id and  date(f.visit_date) = date(:startDate) and f.voided = 0\n" +
-                    "         inner join kenyaemr_etl.etl_drug_event d on d.patient_id = e.patient_id and  date(d.date_started) = date(:startDate) and ifnull(d.voided,0)= 0\n" +
-                    "where e.voided = 0 ";
+            qry = "SELECT f.patient_id, if(f2.patient_id is not null,\n" +
+                    "    TIMESTAMPDIFF(MONTH , date(f.visit_date), date(f.next_appointment_date)), 'R' ) as res\n" +
+                    "FROM kenyaemr_etl.etl_patient_hiv_followup f\n" +
+                    "         inner join kenyaemr_etl.etl_patient_demographics p on p.patient_id = f.patient_id and  p.voided = 0 and p.Gender = ':sex'\n" +
+                    "         inner join kenyaemr_etl.etl_drug_event d on d.patient_id = f.patient_id  and  date(d.date_started) <= date(:startDate)  and ifnull(d.voided,0)= 0\n" +
+                    "         left join kenyaemr_etl.etl_patient_hiv_followup f2 on f.patient_id = f2.patient_id  and  date(f.visit_date) = date(f2.next_appointment_date) and f2.voided = 0\n" +
+                    "where date(f.visit_date) = date(:startDate) and f.voided = 0 ";
         } else {
-            qry = "SELECT e.patient_id, 'X' hasFollowupVisit\n" +
-                    "FROM kenyaemr_etl.etl_hiv_enrollment e\n" +
-                    "         inner join kenyaemr_etl.etl_patient_demographics p on p.patient_id = e.patient_id and  p.voided = 0 \n" +
-                    "         inner join kenyaemr_etl.etl_patient_hiv_followup f on f.patient_id = e.patient_id and  date(f.visit_date) = date(:startDate) and f.voided = 0\n" +
-                    "         inner join kenyaemr_etl.etl_drug_event d on d.patient_id = e.patient_id and  date(d.date_started) = date(:startDate) and ifnull(d.voided,0)= 0\n" +
-                    "where e.voided = 0 ";
+            qry = "SELECT f.patient_id, if(f2.patient_id is not null,\n" +
+                    "    TIMESTAMPDIFF(MONTH , date(f.visit_date), date(f.next_appointment_date)), 'R' ) as res\n" +
+                    "FROM kenyaemr_etl.etl_patient_hiv_followup f\n" +
+                    "         inner join kenyaemr_etl.etl_patient_demographics p on p.patient_id = f.patient_id and  p.voided = 0 \n" +
+                    "         inner join kenyaemr_etl.etl_drug_event d on d.patient_id = f.patient_id  and  date(d.date_started) <= date(:startDate)  and ifnull(d.voided,0)= 0\n" +
+                    "         left join kenyaemr_etl.etl_patient_hiv_followup f2 on f.patient_id = f2.patient_id  and  date(f.visit_date) = date(f2.next_appointment_date) and f2.voided = 0\n" +
+                    "where date(f.visit_date) = date(:startDate) and f.voided = 0 ";
         }
         String ageConditionString = "";
         if (minAge != null && maxAge != null) {
