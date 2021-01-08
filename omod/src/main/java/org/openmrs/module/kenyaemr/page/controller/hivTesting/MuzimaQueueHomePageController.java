cf3c09e106819961213deb450a1b9f522e46675e
makombe <kennedymakombe@gmail.com>
2021-01-08 11:41:15 +0300

    Restore muzima error queque

---
+++ omod/src/main/java/org/openmrs/module/kenyaemr/page/controller/hivTesting/MuzimaQueueHomePageController.java
@@ -0,0 +1,27 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.page.controller.hivTesting;
+
+import org.openmrs.module.kenyaemr.EmrConstants;
+import org.openmrs.module.kenyaui.annotation.AppPage;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.page.PageModel;
+
+/**
+ * Homepage for the Hiv Testing app
+ */
+@AppPage(EmrConstants.APP_HIV_TESTING)
+public class MuzimaQueueHomePageController {
+
+	public String controller(UiUtils ui, PageModel model) {
+
+		return null;
+	}
+}
