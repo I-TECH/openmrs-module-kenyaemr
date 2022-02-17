aaefa53afaa0b61a13e216e0ac3d5589f2c91028
Patrick <gpatryllus@gmail.com>
2022-02-17 12:05:07 +0300

    Added currently in MCH linelist

---
+++ api/src/main/java/org/openmrs/module/kenyaemr/reporting/cohort/definition/pmtct/PatientsOnMCHCohortDefinition.java
@@ -0,0 +1,50 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.kenyaemr.reporting.cohort.definition.pmtct;
+
+import org.openmrs.module.reporting.cohort.definition.BaseCohortDefinition;
+import org.openmrs.module.reporting.common.Localized;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationProperty;
+import org.openmrs.module.reporting.definition.configuration.ConfigurationPropertyCachingStrategy;
+import org.openmrs.module.reporting.evaluation.caching.Caching;
+
+import java.util.Date;
+
+/**
+ * OnMCH cohort definition
+ */
+@Caching(strategy = ConfigurationPropertyCachingStrategy.class)
+@Localized("reporting.PatientsOnMCHCohortDefinition")
+public class PatientsOnMCHCohortDefinition extends BaseCohortDefinition {
+@ConfigurationProperty
+    private Date startate;
+
+@ConfigurationProperty
+    private Date endDate;
+
+    public PatientsOnMCHCohortDefinition() {
+    }
+
+    public Date getStartate() {
+        return startate;
+    }
+
+    public void setStartate(Date startate) {
+        this.startate = startate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+}
