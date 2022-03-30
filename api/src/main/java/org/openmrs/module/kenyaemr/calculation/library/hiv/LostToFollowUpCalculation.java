f5d60580b8850b02d997fd0a8ad20f812632498d
Patrick <gpatryllus@gmail.com>
2022-03-30 12:00:44 +0300

    Updated LostToFollowupCalculation to include discontinued clients with reason=lostToFollowup

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/LostToFollowUpCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/hiv/LostToFollowUpCalculation.java
@@ -47,7 +47,8 @@ import static org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils.daysSi
 
 /**
  * Calculates whether a patient has been lost to follow up. Calculation returns true if patient
- * is alive, enrolled in the HIV program, but hasn't had an encounter in LOST_TO_FOLLOW_UP_THRESHOLD_DAYS days
+ * is alive and discontinued from Hiv program,
+ * Or enrolled in the HIV program, but hasn't had an encounter in LOST_TO_FOLLOW_UP_THRESHOLD_DAYS days = 30 days
  */
 public class LostToFollowUpCalculation extends AbstractPatientCalculation implements PatientFlagCalculation {
 
@@ -62,29 +63,35 @@ public class LostToFollowUpCalculation extends AbstractPatientCalculation implem
 	 * Evaluates the calculation
 	 * @should calculate false for deceased patients
 	 * @should calculate false for patients not in HIV program
-	 * @should calculate false for patients without a HIV Greencard encounter
 	 * @should calculate true for patient with a HIV Greencard encounter and 31 days past since last TCA
+	 * OR
+	 * Discontinued Hiv clients with reason Lost to followup
 	 */
 	@Override
 	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> arg1, PatientCalculationContext context) {
 
 		Program hivProgram = MetadataUtils.existing(Program.class, HivMetadata._Program.HIV);
-		Concept reasonForDiscontinuation = Dictionary.getConcept(Dictionary.REASON_FOR_PROGRAM_DISCONTINUATION);
-		Concept transferout = Dictionary.getConcept(Dictionary.TRANSFERRED_OUT);
 		Set<Integer> alive = Filters.alive(cohort, context);
 		Set<Integer> inHivProgram = Filters.inProgram(hivProgram, alive, context);
-
-		CalculationResultMap lastProgramDiscontinuation = Calculations.lastObs(reasonForDiscontinuation, cohort, context);
 
 		CalculationResultMap ret = new CalculationResultMap();
 		for (Integer ptId : cohort) {
 			boolean lost = false;
 			Integer tcaConcept = 5096;
 			Date tcaDate = null;
+			PatientService patientService = Context.getPatientService();
+			EncounterService encounterService = Context.getEncounterService();
 
+			Concept reasonForDiscontinuation = Dictionary.getConcept(Dictionary.REASON_FOR_PROGRAM_DISCONTINUATION);
+			Concept transferout = Dictionary.getConcept(Dictionary.TRANSFERRED_OUT);
+			Concept discontinued_ltfu = Dictionary.getConcept(Dictionary.LOST_TO_FOLLOWUP);
+			EncounterType hivDiscEncType = MetadataUtils.existing(EncounterType.class, HivMetadata._EncounterType.HIV_DISCONTINUATION);
+			Form hivDiscForm = MetadataUtils.existing(Form.class, HivMetadata._Form.HIV_DISCONTINUATION);
+			Encounter lastHivDiscontinuationEncounter = EmrUtils.lastEncounter(patientService.getPatient(ptId), hivDiscEncType, hivDiscForm);  //last hiv discontinuation encounter
+			EncounterType hivEnrolmentEncounter = encounterService.getEncounterTypeByUuid(HivMetadata._EncounterType.HIV_ENROLLMENT);
+			Encounter lastHivEnrollmentEncounter = EmrUtils.lastEncounter(patientService.getPatient(ptId), hivEnrolmentEncounter);
 			// Is patient alive and in HIV program
 			if (inHivProgram.contains(ptId)) {
-				PatientService patientService = Context.getPatientService();
 				//With Greencard Encounter
 				EncounterType greenCardEncType = MetadataUtils.existing(EncounterType.class, HivMetadata._EncounterType.HIV_CONSULTATION);
 				Form pocHivFollowup = MetadataUtils.existing(Form.class, HivMetadata._Form.HIV_GREEN_CARD);
@@ -95,21 +102,29 @@ public class LostToFollowUpCalculation extends AbstractPatientCalculation implem
 					for (Obs obs : lastFollowUpEncounter.getObs()) {
 						if (obs.getConcept().getConceptId().equals(tcaConcept)) {
 							tcaDate = obs.getValueDatetime();
-							Obs discontinuation = EmrCalculationUtils.obsResultForPatient(lastProgramDiscontinuation, ptId);
 							if (tcaDate != null) {
 								if (daysSince(tcaDate, context) > HivConstants.LOST_TO_FOLLOW_UP_THRESHOLD_DAYS) {
 									lost = true;
-								}
-								if (discontinuation != null && discontinuation.getValueCoded().equals(transferout)) {
-									lost = false;
 								}
 							}
 						}
 					}
 				}
 			}
-			ret.put(ptId, new SimpleResult(lost, this, context));
-
+			// Is patient alive and discontinued from HIV program
+			if (alive.contains(ptId) && lastHivDiscontinuationEncounter != null) {          //these clients are no longer in hiv prog
+				if (lastHivDiscontinuationEncounter.getEncounterDatetime().after(lastHivEnrollmentEncounter.getEncounterDatetime())) {   // check for re-enrollments
+					for (Obs obs : lastHivDiscontinuationEncounter.getObs()) {
+						if (obs.getConcept().equals(reasonForDiscontinuation) && obs.getValueCoded().equals(discontinued_ltfu)) {
+							lost = true;
+						}
+						if (obs.getConcept().equals(reasonForDiscontinuation) && obs.getValueCoded().equals(transferout)) {
+							lost = false;
+						}
+					}
+					ret.put(ptId, new SimpleResult(lost, this, context));
+				}
+			}
 		}
 		return ret;
 	}
