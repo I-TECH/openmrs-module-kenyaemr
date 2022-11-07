07d0ca97b26147a3cb006df027282826868e1eb6
Patrick <gpatryllus@gmail.com>
2022-11-07 14:39:46 +0300

    Updated UPI client verification summary to include current in HIV

--- omod/src/main/java/org/openmrs/module/kenyaemr/page/controller/upi/UpiVerificationHomePageController.java
+++ omod/src/main/java/org/openmrs/module/kenyaemr/page/controller/upi/UpiVerificationHomePageController.java
@@ -10,12 +10,17 @@
 package org.openmrs.module.kenyaemr.page.controller.upi;
 
 import org.openmrs.Patient;
+import org.openmrs.PatientProgram;
 import org.openmrs.PersonAttributeType;
+import org.openmrs.Program;
+import org.openmrs.api.ProgramWorkflowService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.kenyaemr.EmrConstants;
 import org.openmrs.module.kenyaemr.metadata.CommonMetadata;
+import org.openmrs.module.kenyaemr.metadata.HivMetadata;
 import org.openmrs.module.kenyaui.KenyaUiUtils;
 import org.openmrs.module.kenyaui.annotation.AppPage;
+import org.openmrs.module.metadatadeploy.MetadataUtils;
 import org.openmrs.ui.framework.SimpleObject;
 import org.openmrs.ui.framework.UiUtils;
 import org.openmrs.ui.framework.annotation.SpringBean;
@@ -32,9 +37,11 @@ public class UpiVerificationHomePageController {
 
         List<SimpleObject> pendingVerification = new ArrayList<SimpleObject>();
         List<SimpleObject> verified = new ArrayList<SimpleObject>();
+        List<SimpleObject> verifiedOnART = new ArrayList<SimpleObject>();
         PersonAttributeType verificationStatusPA = Context.getPersonService().getPersonAttributeTypeByUuid(CommonMetadata._PersonAttributeType.VERIFICATION_STATUS_WITH_NATIONAL_REGISTRY);
         PersonAttributeType verificationMessagePA = Context.getPersonService().getPersonAttributeTypeByUuid(CommonMetadata._PersonAttributeType.VERIFICATION_MESSAGE_WITH_NATIONAL_REGISTRY);
         List<Patient> allPatients = Context.getPatientService().getAllPatients();
+        Program hivProgram = MetadataUtils.existing(Program.class, HivMetadata._Program.HIV);
 
         for (Patient patient : allPatients) {
             if (patient.getAttribute(verificationStatusPA) != null) {
@@ -55,6 +62,17 @@ public class UpiVerificationHomePageController {
                             .getGivenName(), "middleName", patient.getMiddleName() != null ? patient.getMiddleName() : "", "familyName", patient.getFamilyName(), "birthdate", kenyaUi.formatDate(patient.getBirthdate()), "gender", patient.getGender(), "error", networkError != null ? networkError : "-");
                     verified.add(patientVerifiedObject);
                 }
+                // Has successful verification and has received NUPI and in HIV program
+                ProgramWorkflowService pwfservice = Context.getProgramWorkflowService();
+                List<PatientProgram> programs = pwfservice.getPatientPrograms(patient, hivProgram, null, null, null,null, true);
+                if (programs.size() > 0) {
+                    if (patient.getAttribute(verificationStatusPA).getValue().equals("Yes") || patient.getAttribute(verificationStatusPA).getValue().equals("Verified")) {
+                        SimpleObject patientVerifiedObject = SimpleObject.create("id", patient.getId(), "uuid", patient.getUuid(), "givenName", patient
+                                .getGivenName(), "middleName", patient.getMiddleName() != null ? patient.getMiddleName() : "", "familyName", patient.getFamilyName(), "birthdate", kenyaUi.formatDate(patient.getBirthdate()), "gender", patient.getGender(), "error", networkError != null ? networkError : "-");
+                        verifiedOnART.add(patientVerifiedObject);
+                    }
+                }
+
 
             }
         }
@@ -62,6 +80,7 @@ public class UpiVerificationHomePageController {
         model.put("patientPendingListSize", pendingVerification.size());
         model.put("patientVerifiedList", ui.toJson(verified));
         model.put("patientVerifiedListSize", verified.size());
+        model.put("patientVerifiedOnARTListSize", verifiedOnART.size());
         model.put("totalAttemptedVerification", pendingVerification.size() + verified.size());
     }
 }
