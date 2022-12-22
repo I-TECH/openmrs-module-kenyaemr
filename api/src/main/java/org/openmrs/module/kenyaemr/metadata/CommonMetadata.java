24e0e9eb26f09b51ad59b09c8be2a5cdd6f0b65b
Patrick <gpatryllus@gmail.com>
2022-12-22 17:29:34 +0300

    Added Prisoner ID personIdentifier and used it to verify if global property for facility handles prisoners

--- api/src/main/java/org/openmrs/module/kenyaemr/metadata/CommonMetadata.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/metadata/CommonMetadata.java
@@ -99,6 +99,7 @@ public class CommonMetadata extends AbstractMetadataBundle {
 		public static final String ALIEN_ID_NUMBER = Metadata.IdentifierType.ALIEN_ID_NUMBER;
 		public static final String DRIVING_LICENSE = Metadata.IdentifierType.DRIVING_LICENSE;
 		public static final String RECENCY_TESTING_ID = Metadata.IdentifierType.RECENCY_TESTING_ID;
+		public static final String PRISONER_ID = Metadata.IdentifierType.PRISONER_ID;
 	}
 
 	public static final class _PersonAttributeType {
@@ -255,6 +256,8 @@ public class CommonMetadata extends AbstractMetadataBundle {
 				null, LocationBehavior.NOT_USED, false, _PatientIdentifierType.DRIVING_LICENSE));
 		install(patientIdentifierType("Recency Testing ID", "Recency Testing ID", "", "Allows for alphanumeric format",
 				null, LocationBehavior.NOT_USED, false, _PatientIdentifierType.RECENCY_TESTING_ID));
+		install(patientIdentifierType("Prisoner ID", "Prisoner ID", "", "Allows for alphanumeric format",
+				null, LocationBehavior.NOT_USED, false, _PatientIdentifierType.PRISONER_ID));
 
 
 		install(personAttributeType("Telephone contact", "Telephone contact number",
