12817cfe7b5ddc997bbe20501dfbe4b146f2d12c
njorocs <njorocs@gmail.com>
2023-03-15 20:24:25 +0300

    KHP3-2426:Early Infant Diagnosis (EID) interval alerts

--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NeedsAntibodyTestCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NeedsAntibodyTestCalculation.java
@@ -13,20 +13,15 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.joda.time.Months;
-import org.openmrs.Concept;
-import org.openmrs.Obs;
-import org.openmrs.Person;
-import org.openmrs.Program;
+import org.openmrs.*;
+import org.openmrs.api.OrderService;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
-import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
-import org.openmrs.module.kenyacore.calculation.BooleanResult;
-import org.openmrs.module.kenyacore.calculation.Calculations;
-import org.openmrs.module.kenyacore.calculation.Filters;
-import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
+import org.openmrs.module.kenyacore.calculation.*;
 import org.openmrs.module.kenyaemr.Dictionary;
 import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
+import org.openmrs.module.kenyaemr.calculation.library.hiv.PendingViralLoadResultCalculation;
 import org.openmrs.module.kenyaemr.metadata.MchMetadata;
 import org.openmrs.module.metadatadeploy.MetadataUtils;
 
@@ -35,64 +30,95 @@ import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
+import static org.openmrs.module.kenyaemrorderentry.util.Utils.getLatestObs;
+
 /**
  * Determines whether a child at 9 months and above has had antibody test
  */
 public class NeedsAntibodyTestCalculation extends AbstractPatientCalculation implements PatientFlagCalculation {
-	protected static final Log log = LogFactory.getLog(NeedsPcrTestCalculation.class);
-	/**
-	 * @see org.openmrs.module.kenyacore.calculation.PatientFlagCalculation#getFlagMessage()
-	 */
-	@Override
-	public String getFlagMessage() {
-		return "Due For Antibody Test";
-	}
+    protected static final Log log = LogFactory.getLog(NeedsPcrTestCalculation.class);
 
-	/**
-	 * @see org.openmrs.calculation.patient.PatientCalculation#evaluate(java.util.Collection, java.util.Map, org.openmrs.calculation.patient.PatientCalculationContext)
-	 */
-	@Override
-	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
+    /**
+     * @see org.openmrs.module.kenyacore.calculation.PatientFlagCalculation#getFlagMessage()
+     */
+    @Override
+    public String getFlagMessage() {
+        return flagMsg.toString();
+    }
 
-		Program mchcsProgram = MetadataUtils.existing(Program.class, MchMetadata._Program.MCHCS);
+    StringBuilder flagMsg = new StringBuilder();
 
-		// Get all patients who are alive and in MCH-CS program
-		Set<Integer> alive = Filters.alive(cohort, context);
-		Set<Integer> inMchcsProgram = Filters.inProgram(mchcsProgram, alive, context);
+    /**
+     * @see org.openmrs.calculation.patient.PatientCalculation#evaluate(java.util.Collection, java.util.Map, org.openmrs.calculation.patient.PatientCalculationContext)
+     */
+    @Override
+    public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
 
-		//CalculationResultMap ages = Calculations.ages(cohort, context);
+        Program mchcsProgram = MetadataUtils.existing(Program.class, MchMetadata._Program.MCHCS);
 
-		// Get whether the child is HIV Exposed
-		CalculationResultMap lastChildHivStatus = Calculations.lastObs(Dictionary.getConcept(Dictionary.CHILDS_CURRENT_HIV_STATUS), cohort, context);
-		CalculationResultMap lastHivRapidTest1 = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_RAPID_TEST_1_QUALITATIVE), cohort, context);
+        // Get all patients who are alive and in MCH-CS program
+        Set<Integer> alive = Filters.alive(cohort, context);
+        Set<Integer> inMchcsProgram = Filters.inProgram(mchcsProgram, alive, context);
 
-		Concept hivExposedUnknown = Dictionary.getConcept(Dictionary.UNKNOWN);
-		Concept hivExposed = Dictionary.getConcept(Dictionary.EXPOSURE_TO_HIV);
-		CalculationResultMap ret = new CalculationResultMap();
+        //CalculationResultMap ages = Calculations.ages(cohort, context);
 
-		for (Integer ptId : cohort) {
-			boolean needsAntibody = false;
+        // Get whether the child is HIV Exposed
+        CalculationResultMap lastChildHivStatus = Calculations.lastObs(Dictionary.getConcept(Dictionary.CHILDS_CURRENT_HIV_STATUS), cohort, context);
+        CalculationResultMap lastHivRapidTest1 = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_RAPID_TEST_1_QUALITATIVE), cohort, context);
+        Set<Integer> pendingDNARapidTestResults = CalculationUtils.patientsThatPass(calculate(new PendingDNAPCRRapidTestResultCalculation(), cohort, context));
+        Concept hivExposedUnknown = Dictionary.getConcept(Dictionary.UNKNOWN);
+        Concept hivExposed = Dictionary.getConcept(Dictionary.EXPOSURE_TO_HIV);
+        CalculationResultMap ret = new CalculationResultMap();
+        OrderService orderService = Context.getOrderService();
+        Concept NEGATIVE = Dictionary.getConcept(Dictionary.NEGATIVE);
+        Concept PCR_12_MONTHS = Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION);
+        Concept AB_18_MONTHS = Dictionary.getConcept(Dictionary.RAPID_HIV_ANTIBODY_TEST_AT_18_MONTHS);
+        Concept AB_6_MONTHS_AFTER_CESSATION_OF_BF = Dictionary.getConcept(Dictionary.AB_TEST_6_WEEKS_AFTER_CESSATION_OF_BREASTFEEDING);
 
-			if (inMchcsProgram.contains(ptId)) {
-				//Integer ageInMonths = ((Age) ages.get(ptId).getValue()).getFullMonths();
-				Obs hivStatusObs = EmrCalculationUtils.obsResultForPatient(lastChildHivStatus, ptId);
-				Obs rapidTest1 = EmrCalculationUtils.obsResultForPatient(lastHivRapidTest1, ptId);
+        Concept INFANT_NOT_BREASTFEEDING = Dictionary.getConcept(Dictionary.INFANT_NOT_BREASTFEEDING);
 
-				Person person = Context.getPersonService().getPerson(ptId);
+        CalculationResultMap lastPcrTestQualitative = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION_QUALITATIVE), cohort, context);
+        CalculationResultMap lastBreastFeedingStatus = Calculations.lastObs(Dictionary.getConcept(Dictionary.INFANT_FEEDING_METHOD), cohort, context);
 
-				if (hivStatusObs != null && (hivStatusObs.getValueCoded().equals(hivExposedUnknown) || hivStatusObs.getValueCoded().equals(hivExposed)) && getAge(person.getBirthdate(), context.getNow()) >= 18 && rapidTest1 == null) {
-					needsAntibody = true;
-				}
-			}
-			ret.put(ptId, new BooleanResult(needsAntibody, this, context));
+        for (Integer ptId : cohort) {
+            boolean needsAntibody = false;
+            Order order = new Order();
 
-		}
-		return ret;
-	}
+            Obs pcrTestObsQual = EmrCalculationUtils.obsResultForPatient(lastPcrTestQualitative, ptId);
+            Obs latestBFStatus = EmrCalculationUtils.obsResultForPatient(lastBreastFeedingStatus, ptId);
 
-	Integer getAge(Date birtDate, Date context) {
-		DateTime d1 = new DateTime(birtDate.getTime());
-		DateTime d2 = new DateTime(context.getTime());
-		return Months.monthsBetween(d1, d2).getMonths();
-	}
+            Date obsBFStatusDate = latestBFStatus != null && latestBFStatus.getValueCoded().equals(INFANT_NOT_BREASTFEEDING) ? latestBFStatus.getObsDatetime() : null;
+
+
+            if (inMchcsProgram.contains(ptId) && !pendingDNARapidTestResults.contains(ptId)) {
+                //Integer ageInMonths = ((Age) ages.get(ptId).getValue()).getFullMonths();
+                Obs hivStatusObs = EmrCalculationUtils.obsResultForPatient(lastChildHivStatus, ptId);
+                Obs rapidTest1 = EmrCalculationUtils.obsResultForPatient(lastHivRapidTest1, ptId);
+
+                if (pcrTestObsQual != null) {
+                    Integer orderId = pcrTestObsQual.getOrder().getOrderId();
+                    order = orderService.getOrder(orderId);
+                }
+
+                Person person = Context.getPersonService().getPerson(ptId);
+                if (hivStatusObs != null && pcrTestObsQual != null && order != null && (hivStatusObs.getValueCoded().equals(hivExposedUnknown) || hivStatusObs.getValueCoded().equals(hivExposed)) && pcrTestObsQual.getValueCoded() == NEGATIVE && !order.getOrderReason().equals(AB_18_MONTHS) && order.getOrderReason().equals(PCR_12_MONTHS) && getAge(person.getBirthdate(), context.getNow()) >= 18) {
+                    needsAntibody = true;
+                    flagMsg.append("Due for month-18 Rapid AB test");
+                } else if (hivStatusObs != null && pcrTestObsQual != null && obsBFStatusDate != null && order != null && (hivStatusObs.getValueCoded().equals(hivExposedUnknown) || hivStatusObs.getValueCoded().equals(hivExposed)) && !order.getOrderReason().equals(AB_6_MONTHS_AFTER_CESSATION_OF_BF) && pcrTestObsQual.getValueCoded() == NEGATIVE && NeedsPcrTestCalculation.getAgeInWeeks(obsBFStatusDate, context.getNow()) >= 6) {
+                    needsAntibody = true;
+                    flagMsg.append("Due for week-6 Rapid AB test after cessation of breastfeeding");
+                }
+
+            }
+            ret.put(ptId, new BooleanResult(needsAntibody, this, context));
+
+        }
+        return ret;
+    }
+
+    Integer getAge(Date birtDate, Date context) {
+        DateTime d1 = new DateTime(birtDate.getTime());
+        DateTime d2 = new DateTime(context.getTime());
+        return Months.monthsBetween(d1, d2).getMonths();
+    }
 }
--- api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NeedsPcrTestCalculation.java
+++ api/src/main/java/org/openmrs/module/kenyaemr/calculation/library/mchcs/NeedsPcrTestCalculation.java
@@ -14,18 +14,13 @@ import org.apache.commons.logging.LogFactory;
 import org.joda.time.DateTime;
 import org.joda.time.Months;
 import org.joda.time.Weeks;
-import org.openmrs.Concept;
-import org.openmrs.Obs;
-import org.openmrs.Person;
-import org.openmrs.Program;
+import org.openmrs.*;
+import org.openmrs.api.OrderService;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
-import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
-import org.openmrs.module.kenyacore.calculation.BooleanResult;
-import org.openmrs.module.kenyacore.calculation.Calculations;
-import org.openmrs.module.kenyacore.calculation.Filters;
-import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
+import org.openmrs.module.kenyacore.calculation.*;
 import org.openmrs.module.kenyaemr.Dictionary;
 import org.openmrs.module.kenyaemr.calculation.EmrCalculationUtils;
 import org.openmrs.module.kenyaemr.metadata.MchMetadata;
@@ -40,71 +35,95 @@ import java.util.Set;
  * Determines whether a child at 6 week and above has had PCR test
  */
 public class NeedsPcrTestCalculation extends AbstractPatientCalculation implements PatientFlagCalculation {
-	protected static final Log log = LogFactory.getLog(NeedsPcrTestCalculation.class);
-	/**
-	 * @see org.openmrs.module.kenyacore.calculation.PatientFlagCalculation#getFlagMessage()
-	 */
-	@Override
-	public String getFlagMessage() {
-		return "Due For PCR Test";
-	}
+    protected static final Log log = LogFactory.getLog(NeedsPcrTestCalculation.class);
 
-	/**
-	 * @see org.openmrs.calculation.patient.PatientCalculation#evaluate(java.util.Collection, java.util.Map, org.openmrs.calculation.patient.PatientCalculationContext)
-	 */
-	@Override
-	public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
+    /**
+     * @see org.openmrs.module.kenyacore.calculation.PatientFlagCalculation#getFlagMessage()
+     */
+    @Override
+    public String getFlagMessage() {
+        return flagMsg.toString();
+    }
 
-		Program mchcsProgram = MetadataUtils.existing(Program.class, MchMetadata._Program.MCHCS);
+    StringBuilder flagMsg = new StringBuilder();
 
-		// Get all patients who are alive and in MCH-CS program
-		Set<Integer> alive = Filters.alive(cohort, context);
-		Set<Integer> inMchcsProgram = Filters.inProgram(mchcsProgram, alive, context);
+    /**
+     * @see org.openmrs.calculation.patient.PatientCalculation#evaluate(java.util.Collection, java.util.Map, org.openmrs.calculation.patient.PatientCalculationContext)
+     */
+    @Override
+    public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues, PatientCalculationContext context) {
 
-		// Get whether the child is HIV Exposed
-		CalculationResultMap lastChildHivStatus = Calculations.lastObs(Dictionary.getConcept(Dictionary.CHILDS_CURRENT_HIV_STATUS), cohort, context);
-		CalculationResultMap lastPcrTestReaction = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION), cohort, context);
-		CalculationResultMap lastPcrTestQualitative = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION_QUALITATIVE), cohort, context);
+        Program mchcsProgram = MetadataUtils.existing(Program.class, MchMetadata._Program.MCHCS);
 
-		Concept hivExposed = Dictionary.getConcept(Dictionary.EXPOSURE_TO_HIV);
+        // Get all patients who are alive and in MCH-CS program
+        Set<Integer> alive = Filters.alive(cohort, context);
+        Set<Integer> inMchcsProgram = Filters.inProgram(mchcsProgram, alive, context);
 
-		CalculationResultMap ret = new CalculationResultMap();
+        // Get whether the child is HIV Exposed
+        CalculationResultMap lastChildHivStatus = Calculations.lastObs(Dictionary.getConcept(Dictionary.CHILDS_CURRENT_HIV_STATUS), cohort, context);
+        CalculationResultMap lastPcrTestReaction = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION), cohort, context);
+        CalculationResultMap lastPcrTestQualitative = Calculations.lastObs(Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION_QUALITATIVE), cohort, context);
+        Set<Integer> pendingDNARapidTestResults = CalculationUtils.patientsThatPass(calculate(new PendingDNAPCRRapidTestResultCalculation(), cohort, context));
 
-		for (Integer ptId : cohort) {
+        Concept NEGATIVE = Dictionary.getConcept(Dictionary.NEGATIVE);
+        Concept hivExposed = Dictionary.getConcept(Dictionary.EXPOSURE_TO_HIV);
+        Concept PCR_6_WEEKS = Dictionary.getConcept(Dictionary.HIV_RAPID_TEST_1_QUALITATIVE);
+        Concept PCR_6_MONTHS = Dictionary.getConcept(Dictionary.HIV_RAPID_TEST_2_QUALITATIVE);
+        Concept PCR_12_MONTHS = Dictionary.getConcept(Dictionary.HIV_DNA_POLYMERASE_CHAIN_REACTION);
 
-			boolean needsPcr = false;
+        CalculationResultMap ret = new CalculationResultMap();
 
-			// Check if a patient is alive and is in MCHCS program
-			if (inMchcsProgram.contains(ptId)) {
+        OrderService orderService = Context.getOrderService();
+        PatientService patientService = Context.getPatientService();
+        OrderType labOrderType = orderService.getOrderTypeByUuid(OrderType.TEST_ORDER_TYPE_UUID);
 
-				Obs hivStatusObs = EmrCalculationUtils.obsResultForPatient(lastChildHivStatus, ptId);
-				Obs pcrTestObs =  EmrCalculationUtils.obsResultForPatient(lastPcrTestReaction, ptId);
-				Obs pcrTestObsQual =  EmrCalculationUtils.obsResultForPatient(lastPcrTestQualitative, ptId);
+        for (Integer ptId : cohort) {
 
-				//get birth date of this patient
-				Person person = Context.getPersonService().getPerson(ptId);
+            boolean needsPcr = false;
+            Order order = new Order();
+            // Check if a patient is alive and is in MCHCS program
+            if (inMchcsProgram.contains(ptId) && !pendingDNARapidTestResults.contains(ptId)) {
 
-				if (hivStatusObs != null && pcrTestObs == null &&  pcrTestObsQual == null && (hivStatusObs.getValueCoded().equals(hivExposed)) && getAgeInWeeks(person.getBirthdate(), context.getNow()) >= 6 && getAgeInMonths(person.getBirthdate(), context.getNow()) <= 12) {
-					needsPcr = true;
+                Obs hivStatusObs = EmrCalculationUtils.obsResultForPatient(lastChildHivStatus, ptId);
+                Obs pcrTestObs = EmrCalculationUtils.obsResultForPatient(lastPcrTestReaction, ptId);
+                Obs pcrTestObsQual = EmrCalculationUtils.obsResultForPatient(lastPcrTestQualitative, ptId);
 
-				}
+                if (pcrTestObsQual != null) {
+                    Integer orderId = pcrTestObsQual.getOrder().getOrderId();
+                    order = orderService.getOrder(orderId);
+                }
 
-			}
+                //get birth date of this patient
+                Person person = Context.getPersonService().getPerson(ptId);
 
-			ret.put(ptId, new BooleanResult(needsPcr, this, context));
-		}
-		return ret;
-	}
+                if (hivStatusObs != null && pcrTestObsQual == null && (hivStatusObs.getValueCoded().equals(hivExposed))  && !order.getOrderReason().equals(PCR_6_WEEKS) && getAgeInWeeks(person.getBirthdate(), context.getNow()) >= 6) {
+                    needsPcr = true;
+                    flagMsg.append("Due for week-6 PCR test");
+                } else if (hivStatusObs != null && pcrTestObsQual != null && order != null && hivStatusObs.getValueCoded().equals(hivExposed) && pcrTestObsQual.getValueCoded() == NEGATIVE && !order.getOrderReason().equals(PCR_6_MONTHS) && order.getOrderReason().equals(PCR_6_WEEKS) && getAgeInMonths(person.getBirthdate(), context.getNow()) >= 6) {
+                    needsPcr = true;
+                    flagMsg.append("Due for month-6 PCR test");
+                } else if (hivStatusObs != null && pcrTestObsQual != null && order != null && (hivStatusObs.getValueCoded().equals(hivExposed)) && pcrTestObsQual.getValueCoded() == NEGATIVE && !order.getOrderReason().equals(PCR_12_MONTHS) && order.getOrderReason().equals(PCR_6_MONTHS) && getAgeInMonths(person.getBirthdate(), context.getNow()) >= 12) {
+                    needsPcr = true;
+                    flagMsg.append("Due for month-12 PCR test");
+                }
 
-	Integer getAgeInWeeks(Date birtDate, Date context) {
-		DateTime d1 = new DateTime(birtDate.getTime());
-		DateTime d2 = new DateTime(context.getTime());
-		return Weeks.weeksBetween(d1, d2).getWeeks();
-	}
+            }
 
-	Integer getAgeInMonths(Date birtDate, Date context) {
-		DateTime d1 = new DateTime(birtDate.getTime());
-		DateTime d2 = new DateTime(context.getTime());
-		return Months.monthsBetween(d1, d2).getMonths();
-	}
+            ret.put(ptId, new BooleanResult(needsPcr, this, context));
+        }
+
+        return ret;
+    }
+
+   public static Integer getAgeInWeeks(Date birtDate, Date context) {
+        DateTime d1 = new DateTime(birtDate.getTime());
+        DateTime d2 = new DateTime(context.getTime());
+        return Weeks.weeksBetween(d1, d2).getWeeks();
+    }
+
+    Integer getAgeInMonths(Date birtDate, Date context) {
+        DateTime d1 = new DateTime(birtDate.getTime());
+        DateTime d2 = new DateTime(context.getTime());
+        return Months.monthsBetween(d1, d2).getMonths();
+    }
 }
